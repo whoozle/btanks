d8619d7c6843bd8e9696a0058e5fd823a46d28cc
Vladimir Menshakov <whoozle@yandex.ru>
2006-07-24 08:33:44 +0000

    added connect/close methods
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@103 5c0bf991-d514-0410-a65f-ce4832e571f3

--- sdlx/tcp_socket.cpp
+++ sdlx/tcp_socket.cpp
@@ -1,3 +1,45 @@
 #include "tcp_socket.h"
+#include "net_ex.h"
+
 using namespace sdlx;
 
+TCPSocket::TCPSocket() : _sock(NULL) {}
+
+void TCPSocket::listen(const int port) {
+	close();
+	
+	IPaddress ip;
+	
+	if (SDLNet_ResolveHost(&ip, NULL, port) == -1)
+		throw_net(("SDLNet_ResolveHost"));
+	        
+	_sock = SDLNet_TCP_Open(&ip);
+	if (!_sock) {
+		throw_net(("SDLNet_TCP_Open"));
+	}
+}
+
+void TCPSocket::connect(const std::string &host, const int port) {
+	close();
+	
+	IPaddress ip;
+	
+	if(SDLNet_ResolveHost(&ip, host.c_str(), port)==-1) 
+		throw_net(("SDLNet_ResolveHost"));
+	        
+	_sock = SDLNet_TCP_Open(&ip);
+	if(!_sock) 
+		throw_net(("SDLNet_TCP_Open"));	                
+}
+
+
+void TCPSocket::close() {
+	if (_sock == NULL) 
+		return;
+	SDLNet_TCP_Close(_sock);
+	_sock = NULL;
+}
+
+TCPSocket::~TCPSocket() {
+	close();
+}
--- sdlx/tcp_socket.h
+++ sdlx/tcp_socket.h
@@ -2,12 +2,20 @@
 #define __BTANKS_TCPSOCKET_H__
 
 #include <SDL/SDL_net.h>
+#include <string>
 
 namespace sdlx {
 
 class TCPSocket {
 public:
 	TCPSocket();
+	void listen(const int port);
+	void connect(const std::string &host, const int port);
+	void close();
+
+	~TCPSocket();
+private: 
+	::TCPsocket _sock;
 };
 
 }
