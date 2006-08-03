083159b9b6bea1a97a0ff3a0d148bcc08bf3da13
Vladimir Menshakov <whoozle@yandex.ru>
2006-08-03 07:46:44 +0000

    cleaned up commented obsoleted code
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@179 5c0bf991-d514-0410-a65f-ce4832e571f3

--- objects/joyplayer.cpp
+++ objects/joyplayer.cpp
@@ -25,36 +25,6 @@ void JoyPlayer::tick(const float dt) {
 	Player::tick(dt);
 }
 
-/*
-void JoyPlayer::processEvent(const SDL_Event &event) {
-	switch(event.type) {
-		case SDL_JOYAXISMOTION: 
-			//LOG_DEBUG(("%d:%d", event.jaxis.axis, event.jaxis.value));
-			if (event.jaxis.axis == 0) {
-				vx = 0;
-				if (event.jaxis.value >= THRESHOLD)  vx += 1;
-				if (event.jaxis.value <= -THRESHOLD) vx -= 1;
-			}
-
-			if (event.jaxis.axis == 1) {
-				vy = 0;
-				if (event.jaxis.value >= THRESHOLD)  vy += 1;
-				if (event.jaxis.value <= -THRESHOLD) vy -= 1;
-			}
-			//LOG_DEBUG(("(vx, vy) = (%f, %f)", vx, vy));
-		break;
-		case SDL_JOYBUTTONDOWN: 
-			//LOG_DEBUG(("button: %d", event.jbutton.button));
-			switch(event.jbutton.button) {
-				case 0: action.emit("shoot");
-			}
-		break;
-		case SDL_JOYBUTTONUP: 
-		break;
-	}
-}
-*/
-
 JoyPlayer::~JoyPlayer() {
 	_joy.close();
 }
