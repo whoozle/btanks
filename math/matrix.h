3550b19d200969a39f7b775022555da9ceb3a2e6
Vladimir Menshakov <whoozle@yandex.ru>
2006-08-16 07:24:28 +0000

    fixed overflow in get/set methods
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@238 5c0bf991-d514-0410-a65f-ce4832e571f3

--- math/matrix.h
+++ math/matrix.h
@@ -7,25 +7,25 @@
 template <class T> class Matrix {
 public:
 	Matrix() : _w(0), _h(0), _use_default(false) {}
-	Matrix(const int h, const int w): _use_default(false) {
-		setSize(h, w);
+	Matrix(const int h, const int w, const T v = 0): _use_default(false) {
+		setSize(h, w, v);
 	}
 	
 	void useDefault(const T d) { _default = d; _use_default = true; }
 	
-	void setSize(const int h, const int w) {
+	void setSize(const int h, const int w, const T v = 0) {
 		_w = w;
 		_h = h;
 		_data.setSize(w * h * sizeof(T));
 
 		T *ptr = (T*) _data.getPtr();
 		for(int i = 0; i < w*h; ++i) {
-			*ptr++ = 0;
+			*ptr++ = v;
 		}
 	}
 	
 	inline const T get(const int y, const int x) const {
-		if (x < 0 || x >= _w || y < 0 || y > _h) {
+		if (x < 0 || x >= _w || y < 0 || y >= _h) {
 			if (_use_default) 
 				return _default;
 			
@@ -37,7 +37,7 @@ public:
 	}
 	
 	inline void set(const int y, const int x, const T v) {
-		if (x < 0 || x >= _w || y < 0 || y > _h) {
+		if (x < 0 || x >= _w || y < 0 || y >= _h) {
 			if (_use_default)
 				return;
 			throw_ex(("set(%d, %d) is out of bounds", y, x));
