87a3c38f7bc415e692693114b46ac12ec5dc7016
Vladimir Menshakov <whoozle@yandex.ru>
2006-08-22 08:04:34 +0000

    renamed corpse single-pose, class corpse is still available
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@263 5c0bf991-d514-0410-a65f-ce4832e571f3

--- objects/single_pose.cpp
+++ objects/single_pose.cpp
@@ -1,14 +1,14 @@
 #include "object.h"
 #include "resource_manager.h"
 
-class Corpse : public Object {
+class SinglePose : public Object {
 public:
-	Corpse() : Object("corpse", true) {}
+	SinglePose() : Object("single-pose", true) {}
 	virtual Object * clone(const std::string &opt) const;
 	virtual void emit(const std::string &event, const BaseObject * emitter = NULL);
 };
 
-void Corpse::emit(const std::string &event, const BaseObject * emitter) {
+void SinglePose::emit(const std::string &event, const BaseObject * emitter) {
 	if (event == "collision") {
 		if (emitter->classname == "bullet") {
 			hp -= emitter->hp;
@@ -21,15 +21,13 @@ void Corpse::emit(const std::string &event, const BaseObject * emitter) {
 }
 
 
-Object* Corpse::clone(const std::string &opt) const  {
-	Object *a = new Corpse(*this);
+Object* SinglePose::clone(const std::string &opt) const  {
+	Object *a = new SinglePose(*this);
 	ResourceManager->initMe(a, opt);
-/*	a->speed = 0;
-	a->hp = 10;
-	a->ttl = 60;
-*/
+
 	a->play("main", true);
 	return a;
 }
 
-REGISTER_OBJECT("corpse", Corpse, ());
+REGISTER_OBJECT("single-pose", SinglePose, ());
+REGISTER_OBJECT("corpse", SinglePose, ());
