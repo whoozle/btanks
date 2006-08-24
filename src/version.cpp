0b49b75056593f5d32d2fba2247e405cf3954ba6
Vladimir Menshakov <whoozle@yandex.ru>
2006-08-24 10:22:21 +0000

    added version string
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@296 5c0bf991-d514-0410-a65f-ce4832e571f3

--- src/version.cpp
+++ src/version.cpp
@@ -1,6 +1,7 @@
 #include "version.h"
 #ifdef WIN32
 #pragma comment(exestr, "Battle Tanks (c)2006 Battle tanks team. version 0.3." VERSION)
+#pragma comment( user, "Compiled on " __DATE__ " at " __TIME__ ) 
 #endif
 
 const std::string& getVersion() {
