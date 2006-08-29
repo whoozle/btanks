59b599562818a8cf39a679424f539bb4bc19c2b1
Vladimir Menshakov <whoozle@yandex.ru>
2006-08-29 10:21:18 +0000

    fixed flip on gl surfaces
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@328 5c0bf991-d514-0410-a65f-ce4832e571f3

--- sdlx/surface.cpp
+++ sdlx/surface.cpp
@@ -243,11 +243,9 @@ void Surface::update(const int x, const int y, const int w, const int h) {
 
 void Surface::flip() {
 	if ((surface->flags & SDL_OPENGL) == SDL_OPENGL) {
-		update();
 		SDL_GL_SwapBuffers();
-	} else {
-		SDL_Flip(surface);
 	}
+	SDL_Flip(surface);
 }
 
 void Surface::toggleFullscreen() {
