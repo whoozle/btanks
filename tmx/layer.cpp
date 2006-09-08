339c0bf15a0eb0c700fab398e3371448e25b4104
Vladimir Menshakov <whoozle@yandex.ru>
2006-09-08 12:32:20 +0000

    made Layer::get inline
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@407 5c0bf991-d514-0410-a65f-ce4832e571f3

--- tmx/layer.cpp
+++ tmx/layer.cpp
@@ -4,11 +4,3 @@
 Layer::Layer(const long w, const long h, const mrt::Chunk & data, const int impassability) : impassability(impassability), _w(w), _h(h){
 	_data = data;
 }
-
-const long Layer::get(const long x, const long y) const {
-	if (x < 0 || x >= _w || y < 0 || y >= _h) 
-		return 0;
-	
-	register long * ptr = (long *) _data.getPtr();
-	return *(ptr + _w * y + x);
-}
--- tmx/layer.h
+++ tmx/layer.h
@@ -10,7 +10,13 @@ public:
 	const int impassability;
 
 	Layer(const long w, const long h, const mrt::Chunk & data, const int impassability);
-	const long get(const long x, const long y) const;
+
+	inline const long get(const long x, const long y) const {
+		if (x < 0 || x >= _w || y < 0 || y >= _h) 
+			return 0;	
+		return *((long *) _data.getPtr() + _w * y + x);
+	}
+
 private: 
 	mrt::Chunk _data;
 	long _w, _h;
