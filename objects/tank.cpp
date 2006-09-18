45f621e3ef77301a5af8b8035d172dc4154bf0c6
Vladimir Menshakov <whoozle@yandex.ru>
2006-09-18 06:22:41 +0000

    added mine for tank
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@523 5c0bf991-d514-0410-a65f-ce4832e571f3

--- objects/tank.cpp
+++ objects/tank.cpp
@@ -46,9 +46,6 @@ void Tank::emit(const std::string &event, BaseObject * emitter) {
 }
 
 const bool Tank::take(const BaseObject *obj, const std::string &type) {
-	if (obj->classname == "mines") //disable mine for a tank
-		return false; 
-	
 	if (get("missiles")->take(obj, type))
 		return true;
 	return BaseObject::take(obj, type);
