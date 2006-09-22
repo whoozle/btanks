27029f63b48905bf5313e316b738eb157637ccbd
Vladimir Menshakov <whoozle@yandex.ru>
2006-09-22 09:51:49 +0000

    added _send_q handling.
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@594 5c0bf991-d514-0410-a65f-ce4832e571f3

--- net/monitor.cpp
+++ net/monitor.cpp
@@ -3,16 +3,48 @@
 
 Monitor::Task::Task() : data(new mrt::Chunk), pos(0), len(0) {}
 Monitor::Task::Task(const mrt::Chunk &d) : data(new mrt::Chunk(d)), pos(0), len(data->getSize()) {}
-Monitor::Monitor() {}
+void Monitor::Task::clear() { delete data; pos = len = 0; }
+
+Monitor::Monitor() : _running(false) {
+	start();
+}
 
 void Monitor::add(Connection *c) {
+	sdlx::AutoMutex m(_connections_mutex);
 	_connections.push_back(c);
 }
 	
 void Monitor::send(const mrt::Chunk &data) {
-
+	Task *t = new Task(data);
+	
+	sdlx::AutoMutex m(_connections_mutex);
+	_send_q.push_back(t);
 }
 
 const int Monitor::run() {
+	_running = true;
+	while(_running) {
+		int n;
+		{
+			sdlx::AutoMutex m(_connections_mutex);
+			n = _connections.size();
+		}
+		if (n == 0)
+			SDL_Delay(100);
+	}
 	return 0;
+}
+
+
+Monitor::~Monitor() {
+	_running = false;
+	wait();
+	for(TaskQueue::iterator i = _send_q.begin(); i != _send_q.end(); ++i) {
+		(*i)->clear();
+		delete *i;
+	}
+	for(TaskQueue::iterator i = _recv_q.begin(); i != _recv_q.end(); ++i) {
+		(*i)->clear();
+		delete *i;
+	}
 }
--- net/monitor.h
+++ net/monitor.h
@@ -1,8 +1,9 @@
 #ifndef __BTANKS_NET_MONITOR_H__
 #define __BTANKS_NET_MONITOR_H__
 
-#include "sdlx/thread.h"
 #include <deque>
+#include "sdlx/thread.h"
+#include "sdlx/mutex.h"
 
 namespace mrt {
 	class Chunk;
@@ -16,21 +17,30 @@ public:
 	
 	virtual void onRecv(const mrt::Chunk &data) = 0;
 	void send(const mrt::Chunk &data);
+	
+	~Monitor();
 
 private:
+	bool _running;
+	
 	virtual const int run();
 	typedef std::deque<Connection *> ConnectionList;
 	
 	struct Task {
 		Task();
 		Task(const mrt::Chunk &);
+		void clear();
 		
 		mrt::Chunk *data;
 		unsigned int pos;
 		unsigned int len;
 	};
 	
+	typedef std::deque<Task *> TaskQueue;
+	TaskQueue _send_q, _recv_q;
+	
 	ConnectionList _connections;
+	sdlx::Mutex _connections_mutex;
 };
 
 #endif
