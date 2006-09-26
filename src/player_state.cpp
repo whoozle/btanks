5bfbf1a47b6e808ab5c8bf5424d4b980c2e1dec3
Vladimir Menshakov <whoozle@yandex.ru>
2006-09-26 09:04:25 +0000

    fixed invalid player state serialization
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@633 5c0bf991-d514-0410-a65f-ce4832e571f3

--- src/player_state.cpp
+++ src/player_state.cpp
@@ -6,7 +6,7 @@ PlayerState::PlayerState() { clear(); }
 void PlayerState::clear() { left = right = up = down = fire = alt_fire = false; }
 
 void PlayerState::serialize(mrt::Serializator &s) const {
-	int packed = left?1:0 | right?2:0 | up ? 4:0 | down ? 8:0 | fire ? 16:0 | alt_fire ? 32:0;
+	int packed = (left?1:0) | (right?2:0) | (up ? 4:0) | (down ? 8:0) | (fire ? 16:0) | (alt_fire ? 32:0);
 	s.add(packed);
 }
 
