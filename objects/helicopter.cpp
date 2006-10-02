8b3716024ef4b8470f849353b241d1836887668c
Vladimir Menshakov <whoozle@yandex.ru>
2006-10-02 08:19:28 +0000

    added preliminary helicopter-paratrooper-kamikaze support
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@710 5c0bf991-d514-0410-a65f-ce4832e571f3

--- objects/helicopter.cpp
+++ objects/helicopter.cpp
@@ -1,12 +1,17 @@
 #include "object.h"
+#include "alarm.h"
 #include "tmx/map.h"
 #include "mrt/random.h"
 #include "resource_manager.h"
 
+#define SPAWN_RATE 1
+
 class Helicopter : public Object {
 public:
-	Helicopter() : Object("helicopter"), _active(false) {}
+	Helicopter(const std::string &para) :
+		 Object("helicopter"), _active(false), _spawn(SPAWN_RATE, true), _paratrooper(para) {}
 	virtual void calculate(const float dt);
+	virtual void tick(const float dt);
 	virtual Object * clone() const;
 	virtual void emit(const std::string &event, BaseObject * emitter = NULL);
 	void onSpawn();
@@ -15,21 +20,33 @@ public:
 		Object::serialize(s);
 		_next_target.serialize(s);
 		s.add(_active);
+		s.add(_paratrooper);
 	}
 	virtual void deserialize(const mrt::Serializator &s) {
 		Object::deserialize(s);
 		_next_target.deserialize(s);
 		s.get(_active);
+		s.get(_paratrooper);
 	}
 
 private: 
 	v3<float> _next_target;
 	bool _active;
+	Alarm _spawn;
+	std::string _paratrooper;
 };
 
 void Helicopter::onSpawn() {
 	play("main", true);
 }
+
+void Helicopter::tick(const float dt) {
+	Object::tick(dt);
+	if (_active && _spawn.tick(dt)) {
+		spawn(_paratrooper, "paratrooper", v3<float>(0,0,-1), v3<float>());
+	}
+}
+
 
 void Helicopter::calculate(const float dt) {
 	if (!_active && _idle_time > 2.0) { //2 seconds delay before random target
@@ -64,4 +81,4 @@ Object* Helicopter::clone() const  {
 	return new Helicopter(*this);
 }
 
-REGISTER_OBJECT("helicopter", Helicopter, ());
+REGISTER_OBJECT("helicopter-with-kamikazes", Helicopter, ("paratrooper-kamikaze"));
