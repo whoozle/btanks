8a80f9e744885f9833bb76a4ef875e3272d95b5b
Vladimir Menshakov <whoozle@yandex.ru>
2006-10-02 07:45:48 +0000

    renamed tiles, added paratrooper/kamikaze entries in resources
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@709 5c0bf991-d514-0410-a65f-ce4832e571f3

---
+++ objects/paratrooper.cpp
@@ -0,0 +1,52 @@
+#include "object.h"
+#include "resource_manager.h"
+
+class Paratrooper : public Object {
+public:
+	Paratrooper(const std::string &classname, const std::string &spawn_object, const std::string &spawn_animation) : 
+		Object(classname), _spawn_object(spawn_object), _spawn_animation(spawn_animation) { 
+	}
+	virtual void tick(const float dt);
+	virtual Object * clone() const;
+	virtual void onSpawn();
+	virtual void emit(const std::string &event, BaseObject * emitter = NULL);
+	virtual void serialize(mrt::Serializator &s) const {
+		Object::serialize(s);
+		s.add(_spawn_object);
+		s.add(_spawn_animation);
+	}
+	virtual void deserialize(const mrt::Serializator &s) {
+		Object::deserialize(s);
+		s.get(_spawn_object);
+		s.get(_spawn_animation);
+	}
+private:
+	std::string _spawn_object, _spawn_animation;
+};
+
+
+void Paratrooper::tick(const float dt) {
+	Object::tick(dt);
+	if (getState().empty()) {	
+		//LOG_DEBUG(("over"));
+		spawn(_spawn_object, _spawn_animation, v3<float>(), v3<float>());
+		emit("death", this);
+	}
+}
+
+void Paratrooper::onSpawn() {
+	setDirection(0);
+	play("main", false);
+}
+
+void Paratrooper::emit(const std::string &event, BaseObject * emitter) {
+	Object::emit(event, emitter);
+}
+
+
+Object* Paratrooper::clone() const  {
+	Object *a = new Paratrooper(*this);
+	return a;
+}
+
+REGISTER_OBJECT("paratrooper-kamikaze", Paratrooper, ("paratrooper", "kamikaze", "kamikaze"));
