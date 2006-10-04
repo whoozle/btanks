6678705c056832c97bb3f7d2679794fc02475b44
Vladimir Menshakov <whoozle@yandex.ru>
2006-10-04 06:45:09 +0000

    restricted ticking only to existing in the world objects
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@751 5c0bf991-d514-0410-a65f-ce4832e571f3

--- src/world.cpp
+++ src/world.cpp
@@ -464,10 +464,10 @@ void IWorld::tick(ObjectSet &objects, const float dt) {
 		tick(*o, dt);
 		if (o->isDead()) {
 			ObjectMap::iterator m = _id2obj.find(o->_id);
-			if (m != _id2obj.end()) {
-				assert(o == m->second);
-				_id2obj.erase(m);
-			}
+			assert(m != _id2obj.end())
+			assert(o == m->second);
+			_id2obj.erase(m);
+			
 			delete o;
 			objects.erase(i++);
 		} else ++i;
