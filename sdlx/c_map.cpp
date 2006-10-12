6e85d6673ce7874a7ce26945693c259b41419114
Vladimir Menshakov <whoozle@yandex.ru>
2006-10-12 03:36:24 +0000

    added 16 values for interlacing
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@951 5c0bf991-d514-0410-a65f-ce4832e571f3

--- sdlx/c_map.cpp
+++ sdlx/c_map.cpp
@@ -60,11 +60,13 @@ const bool CollisionMap::collides(const sdlx::Rect &src, const CollisionMap *oth
 	int size1 = _data.getSize();
 	int size2 = other->_data.getSize();
 	
-	//int steps = 0;
 
-#define INTERLACE_STEP 8
+//you can play with it, but 8 seems optimal for me.
+#define INTERLACE_STEP 8 
 
-#if INTERLACE_STEP == 8
+#if INTERLACE_STEP == 16
+	int steps_pos[] = {0, 8, 4, 12,  2, 10, 6, 14,  1, 9, 5, 13,  3, 11, 7, 15};	
+#elif INTERLACE_STEP == 8
 	int steps_pos[] = {0, 4, 2, 6, 3, 7, 1, 5};
 #elif INTERLACE_STEP == 4
 	int steps_pos[] = {0, 2, 1, 3};
@@ -72,6 +74,9 @@ const bool CollisionMap::collides(const sdlx::Rect &src, const CollisionMap *oth
 	int steps_pos[] = {0, 1};
 #endif
 
+/*	int steps = 0;
+	int steps_total = (inter_y1 - inter_y0 + 1) * (inter_x1 - inter_x0 + 1);
+*/
 	for(int sy = 0; sy < INTERLACE_STEP; ++sy) 
 	for(int sx = 0; sx < INTERLACE_STEP; ++sx) 
 	for(y = inter_y0 + steps_pos[sy]; y <= inter_y1 ; y += INTERLACE_STEP) {
@@ -92,7 +97,7 @@ const bool CollisionMap::collides(const sdlx::Rect &src, const CollisionMap *oth
 			unsigned bit2 = 1<<(7 - ((x - bx) & 7));
 			
 			if ( (ptr1[pos1]&bit1) != 0 && (ptr2[pos2]&bit2) != 0) {
-				//LOG_DEBUG(("collision detected: %d steps", steps));
+				//LOG_DEBUG(("collision detected: %d of %d : %g%", steps, steps_total, 100.0 * steps / steps_total));
 				return true;
 			}
 			//++steps;
