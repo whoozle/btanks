c4b819dd8c480a30552008e7908fd7c4a0cc40ca
Vladimir Menshakov <whoozle@yandex.ru>
2006-11-17 12:28:53 +0000

    update points only if G() less than old value.
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@1385 5c0bf991-d514-0410-a65f-ce4832e571f3

--- objects/car.cpp
+++ objects/car.cpp
@@ -135,7 +135,6 @@ const bool Car::findPathDone(Way &way) {
 		LOG_DEBUG(("%d: popping vertex. id=%d, x=%d, y=%d, g=%d, h=%d, f=%d", getID(), 
 			current.id, current.id % _pitch, current.id / _pitch, current.g, current.h, current.g + current.h));
 */		
-		_points.insert(PointMap::value_type(current.id, current));
 		_close_list.insert(current.id);
 		const int x = (current.id % _pitch) * _step;
 		const int y = (current.id / _pitch) * _step;
@@ -195,10 +194,15 @@ const bool Car::findPathDone(Way &way) {
 			
 			p.h = h(id, _end_id, _pitch);
 			
+			PointMap::iterator pi = _points.find(id);
+			if (pi != _points.end()) {
+				if (pi->second.g > p.g) {
+					pi->second = p;
+				}
+			} else _points.insert(PointMap::value_type(id, p));
 			
 			
 			if (p.h < 100) {
-				_points.insert(PointMap::value_type(id, p));
 				_end_id = p.id;
 				goto found;
 			}
