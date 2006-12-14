44d5f71a941c2de72f8cc4cd38f617200ed3180a
Vladimir Menshakov <whoozle@yandex.ru>
2006-12-14 10:09:42 +0000

    random tooltip changes it content in time.
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@1549 5c0bf991-d514-0410-a65f-ce4832e571f3

--- objects/tooltip.cpp
+++ objects/tooltip.cpp
@@ -3,13 +3,17 @@
 
 #include "resource_manager.h"
 #include "object.h"
+#include "config.h"
 #include "mrt/random.h"
 #include "sdlx/surface.h"
 
 class Tooltip : public Object {
 public:
-	Tooltip() : Object("tooltip") { impassability = 0; }
+	Tooltip() : Object("tooltip"), _change(true) { impassability = 0; }
 	virtual void onSpawn() {
+		GET_CONFIG_VALUE("objects.random-tooltip.show-time", float, st, 3.0);
+		_change.set(st);
+	
 		const sdlx::Surface * s = getSurface();
 		int w = s->getWidth();
 		int n = (w - 1) / (int)size.x + 1;
@@ -19,11 +23,20 @@ public:
 		play("main", true);
 	}
 	
+	virtual void tick(const float dt) {
+		Object::tick(dt);
+		if (_change.tick(dt)) {
+			Object::setDirection(mrt::random(getDirectionsNumber()));
+		}
+	}
+	
 	virtual void setDirection(const int dir) {}
 
 	virtual Object * clone() const {
 		return new Tooltip(*this);
 	}
+private: 
+	Alarm _change;
 };
 
 REGISTER_OBJECT("random-tooltip", Tooltip, ());
