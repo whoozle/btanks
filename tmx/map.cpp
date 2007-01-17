3f3d2603045f784371480697e42d800451880b12
Vladimir Menshakov <whoozle@yandex.ru>
2007-01-17 08:51:53 +0000

    fixed outline support
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@1761 5c0bf991-d514-0410-a65f-ce4832e571f3

--- tmx/map.cpp
+++ tmx/map.cpp
@@ -95,15 +95,25 @@ const int IMap::getImpassability(const Object *obj, const v3<int>&pos, v3<int> *
 		int layer_im = layer->impassability;
 
 		if (hidden && l->first > obj_z) {
-			if (((hidden_mask & 1) == 0) && hiddenBy(obj, dx1, dy1, layer->getVisibilityMap(xt1, yt1)))
-				hidden_mask |= 1;
-			if (yt1 != yt2 && ((hidden_mask & 2) == 0) && hiddenBy(obj, dx1, dy2, layer->getVisibilityMap(xt1, yt2)))
-				hidden_mask |= 2;
+			if (((hidden_mask & 1) == 0)) {
+				if (hiddenBy(obj, dx1, dy1, layer->getVisibilityMap(xt1, yt1)) ||!collides(obj, dx1, dy1, &_full_tile))
+					hidden_mask |= 1;
+			}
+			
+			if (yt1 != yt2 && ((hidden_mask & 2) == 0)) {
+				if (hiddenBy(obj, dx1, dy2, layer->getVisibilityMap(xt1, yt2)) || !collides(obj, dx1, dy2, &_full_tile))
+					hidden_mask |= 2;
+			}
+			
 			if (xt1 != xt2) {
-				if (((hidden_mask & 4) == 0) && hiddenBy(obj, dx2, dy1, layer->getVisibilityMap(xt2, yt1)))
-					hidden_mask |= 4;
-				if (yt1 != yt2 && ((hidden_mask & 8) == 0) && hiddenBy(obj, dx2, dy2, layer->getVisibilityMap(xt2, yt2)))
-					hidden_mask |= 8;
+				if (((hidden_mask & 4) == 0)) {
+					if (hiddenBy(obj, dx2, dy1, layer->getVisibilityMap(xt2, yt1)) || !collides(obj, dx2, dy1, &_full_tile))
+						hidden_mask |= 4;
+				}
+				if (yt1 != yt2 && ((hidden_mask & 8) == 0)) {
+					if (hiddenBy(obj, dx2, dy2, layer->getVisibilityMap(xt2, yt2)) || !collides(obj, dx2, dy2, &_full_tile))
+						hidden_mask |= 8;
+				}
 			}
 		}
 
