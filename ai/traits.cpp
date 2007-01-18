4514bad0a0d133d14221215cbaaf70d45b237447
Vladimir Menshakov <whoozle@yandex.ru>
2007-01-18 07:16:22 +0000

    added license to files.
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@1771 5c0bf991-d514-0410-a65f-ce4832e571f3

--- ai/traits.cpp
+++ ai/traits.cpp
@@ -3,6 +3,25 @@
 #include "mrt/logger.h"
 #include <assert.h>
 
+/* Battle Tanks Game
+ * Copyright (C) 2006 Battle Tanks team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
 using namespace ai;
 
 const float Traits::get(const std::string &value, const std::string & object, const float hint1, const float hint2) {
