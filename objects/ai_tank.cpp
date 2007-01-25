0fd9adf0956bc1d8adb74b93d83e7a18f0fd730a
Vladimir Menshakov <whoozle@yandex.ru>
2007-01-25 08:32:13 +0000

    ai tank stub.
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@1917 5c0bf991-d514-0410-a65f-ce4832e571f3

---
+++ objects/ai_tank.cpp
@@ -0,0 +1,96 @@
+/* Battle Tanks Game
+ * Copyright (C) 2006-2007 Battle Tanks team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "tank.h"
+#include "mrt/exception.h"
+#include "resource_manager.h"
+#include "config.h"
+
+#include "ai/base.h" 
+
+class AITank:  public Tank, public ai::Base {
+public: 
+	AITank() :  Object("player"), Tank("player") {}
+//	~AITank();
+	virtual void onSpawn();
+	virtual void calculate(const float dt);
+
+	virtual Object * clone() const { return new AITank(*this); }
+
+	virtual const std::string getWeapon(const int idx) const;
+	virtual const int getWeaponAmount(const int idx) const;
+private: 
+
+};
+
+const std::string AITank::getWeapon(const int idx) const {
+	switch(idx) {
+	case 0: 
+	case 1:
+		return get( idx ? "alt-mod": "mod")->getType();
+	default: 
+		throw_ex(("weapon %d doesnt supported", idx));
+	}
+}
+
+const int AITank::getWeaponAmount(const int idx) const{
+	switch(idx) {
+	case 0: 
+		return -1;
+	case 1:
+		return get("mod")->getCount();
+	default: 
+		throw_ex(("weapon %d doesnt supported", idx));
+	}
+}
+
+
+void AITank::onSpawn() {
+	removeOwner(-42);
+
+	addEnemyClass("player");
+	addEnemyClass("trooper");
+	addEnemyClass("kamikaze");
+	
+	addBonusName("heal");
+	addBonusName("megaheal");
+
+	//primary weapon	
+	addBonusName("dispersion-bullets-item");
+	addBonusName("ricochet-bullets-item");
+
+	//secondary weapon
+	addBonusName("guided-missiles-item");
+	addBonusName("dumb-missiles-item");
+	addBonusName("nuke-missiles-item");
+	addBonusName("boomerang-missiles-item");
+	addBonusName("stun-missiles-item");
+	addBonusName("mines-item");
+
+	ai::Base::onSpawn();
+	Tank::onSpawn();
+}
+
+void AITank::calculate(const float dt) {
+	ai::Base::calculate(dt);
+	
+	GET_CONFIG_VALUE("objects.launcher.rotation-time", float, rt, 0.07);
+	limitRotation(dt, rt, true, false);
+}
+
+REGISTER_OBJECT("ai-tank", AITank, ());
