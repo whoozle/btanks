32ad994e2acbb762e0df82569689ee40340403fd
Vladimir Menshakov <whoozle@yandex.ru>
2007-01-31 11:08:20 +0000

    added missed file
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@2067 5c0bf991-d514-0410-a65f-ce4832e571f3

---
+++ objects/trooper.h
@@ -0,0 +1,56 @@
+#ifndef BTANKS_TROOPER_H_
+#define BTANKS_TROOPER_H_
+
+/* Battle Tanks Game
+ * Copyright (C) 2006-2007 Battle Tanks team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+#include "object.h"
+#include "alarm.h"
+
+class Trooper : public Object {
+public:
+	Trooper(const std::string &classname, const std::string &object, const bool aim_missiles) : 
+		Object(classname), _object(object), _aim_missiles(aim_missiles), _fire(false) {}
+	
+	virtual void tick(const float dt);
+
+	virtual Object * clone() const;
+	virtual void onSpawn();
+	virtual void emit(const std::string &event, Object * emitter = NULL);
+
+	virtual void serialize(mrt::Serializator &s) const {
+		Object::serialize(s);
+		s.add(_object);
+		s.add(_aim_missiles);
+		_fire.serialize(s);
+	}
+	virtual void deserialize(const mrt::Serializator &s) {
+		Object::deserialize(s);
+		s.get(_object);
+		s.get(_aim_missiles);
+		_fire.deserialize(s);
+	}	
+
+protected: 
+	std::string _object;
+	bool _aim_missiles;
+	Alarm _fire;
+};
+
+#endif
