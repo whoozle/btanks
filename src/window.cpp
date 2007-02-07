481882d8b2150d4e8c3576468c471058f8000c90
Vladimir Menshakov <whoozle@yandex.ru>
2007-02-07 09:01:51 +0000

    added check for GetAttribute
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@2242 5c0bf991-d514-0410-a65f-ce4832e571f3

--- src/window.cpp
+++ src/window.cpp
@@ -179,18 +179,19 @@ void Window::init(const int argc, char *argv[]) {
 		_window.setVideoMode(w, h, bits, flags );
 
 		r = 0;
-		SDL_GL_GetAttribute( SDL_GL_ACCELERATED_VISUAL, &r);
-		LOG_DEBUG(("SDL_GL_ACCELERATED_VISUAL = %d", r));
+		if (SDL_GL_GetAttribute( SDL_GL_ACCELERATED_VISUAL, &r) == 0) {
+			LOG_DEBUG(("SDL_GL_ACCELERATED_VISUAL = %d", r));
 
 		
-		if (!force_soft && r != 1) {
-			throw_ex(("Looks like you don't have a graphics card that is good enough.\n"
-			"Please ensure that your graphics card supports OpenGL and the latest drivers are installed.\n" 
-			"Try --force-soft-gl switch to enable sofware GL renderer."
-			"Or use --no-gl to switch disable GL renderer completely."
-			));
-		}
-		
+			if (!force_soft && r != 1) {
+				throw_ex(("Looks like you don't have a graphics card that is good enough.\n"
+				"Please ensure that your graphics card supports OpenGL and the latest drivers are installed.\n" 
+				"Try --force-soft-gl switch to enable sofware GL renderer."
+				"Or use --no-gl to switch disable GL renderer completely."
+				));
+			}
+		} else LOG_WARN(("SDL_GL_GetAttribute( SDL_GL_ACCELERATED_VISUAL) failed: %s", SDL_GetError()));
+
 	} else {
 		_window.setVideoMode(w, h, bits, flags);
 	}
