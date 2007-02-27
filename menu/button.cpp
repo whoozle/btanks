e064dadfeba6cb2512f11f5c9a6724a25f793baf
Vladimir Menshakov <whoozle@yandex.ru>
2007-02-27 12:16:02 +0000

    added button control
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@2457 5c0bf991-d514-0410-a65f-ce4832e571f3

---
+++ menu/button.cpp
@@ -0,0 +1,30 @@
+#include "button.h"
+#include "sdlx/font.h"
+#include "resource_manager.h"
+
+Button::Button(const std::string &font, const std::string &label) : _font(ResourceManager->loadFont(font, true)), _label(label), _clicked(false) {
+	_w = _font->render(NULL, 0, 0, label);
+	_background.init("menu/background_box.png", _w + 32, _font->getHeight() + 16);
+}
+
+
+void Button::render(sdlx::Surface& surface, int x, int y) {
+	_background.render(surface, x, y);
+	
+	_font->render(surface, x + (_background.w - _w) / 2, y + (_background.h - _font->getHeight()) / 2, _label);
+}
+
+void Button::reset() {
+	_clicked = false;
+}
+
+bool Button::onMouse(const int button, const bool pressed, const int x, const int y) {
+	_clicked = true;
+	return true;
+}
+
+void Button::getSize(int &w, int &h) const {
+	w = _background.w;
+	h = _background.h;
+}
+
---
+++ menu/button.h
@@ -0,0 +1,31 @@
+#ifndef BTANKS_MENU_Button_H__
+#define BTANKS_MENU_Button_H__
+
+namespace sdlx {
+class Surface;
+class Font;
+}
+
+#include <string>
+#include "control.h"
+#include "box.h"
+
+class Button : public Control {
+public: 
+	Button(const std::string &font, const std::string &label);
+	void getSize(int &w, int &h) const;
+	virtual void render(sdlx::Surface& surface, const int x, const int y);
+	const bool clicked() const { return _clicked; };
+	void reset();
+	virtual bool onMouse(const int button, const bool pressed, const int x, const int y);
+	
+private: 
+	int _w;
+	Box _background;
+	const sdlx::Font * _font;
+	const std::string _label;
+	bool _clicked;
+};
+
+#endif
+
