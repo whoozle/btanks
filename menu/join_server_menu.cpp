13dd171a1edc3555ab46254312e4077ad83801dd
Vladimir Menshakov <whoozle@yandex.ru>
2007-03-02 09:45:08 +0000

    aligned join to center.
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@2519 5c0bf991-d514-0410-a65f-ce4832e571f3

--- menu/join_server_menu.cpp
+++ menu/join_server_menu.cpp
@@ -8,34 +8,35 @@
 
 JoinServerMenu::JoinServerMenu(MainMenu *parent, const int w, const int h) : _parent(parent) {
 	_back = new Button("big", "BACK");
-	int bw, bh, xp;
+	_add = new Button("big", "ADD");
+	_scan = new Button("big", "SCAN");
+	_join = new Button("big", "JOIN");
+	_upper_box = new UpperBox(500, 80, true);
+
+	int bw, bh, xp, base_x = w / 6;
 	_back->getSize(bw, bh);
-	add(sdlx::Rect(xp = 16, h - 96, bw, bh), _back);
+	add(sdlx::Rect(xp = base_x - 64, h - 96, bw, bh), _back);
 	xp += 16 + bw;
 	
-	_add = new Button("big", "ADD");
 	_add->getSize(bw, bh);
 	add(sdlx::Rect(xp, h - 96, bw, bh), _add);
 	xp += 16 + bw;
 
-	_scan = new Button("big", "SCAN");
 	_scan->getSize(bw, bh);
 	add(sdlx::Rect(xp, h - 96, bw, bh), _scan);
 	
-	_join = new Button("big", "JOIN");
 	_join->getSize(bw, bh);
 	add(sdlx::Rect(w - 64 - bw, h - 96, bw, bh), _join);
 	
-	_upper_box = new UpperBox(500, 80, true);
 	sdlx::Rect r((w - _upper_box->w) / 2, 32, _upper_box->w, _upper_box->h);
 	add(r, _upper_box);
 
-	sdlx::Rect list_pos(0, 128, (w - 64)/3, h - 256);
+	sdlx::Rect list_pos(base_x, 128, (w - 64)/3, h - 256);
 
 	_hosts = new ScrollList(list_pos.w, list_pos.h);
 	add(list_pos, _hosts);
 	
-	sdlx::Rect map_pos(list_pos.w + 16, 128, (w - 64) / 3, h - 256);
+	sdlx::Rect map_pos(list_pos.x + list_pos.w + 32, 128, (w - 64) / 3, h - 256);
 
 	_details = new MapDetails(map_pos.w, map_pos.h);
 	add(map_pos, _details);	
