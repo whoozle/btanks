19e3153dee401a0872b8b8288ccca3e386ffc39b
Vladimir Menshakov <whoozle@yandex.ru>
2007-03-07 08:57:16 +0000

    implemented save/reload methods
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@2557 5c0bf991-d514-0410-a65f-ce4832e571f3

--- menu/control_picker.cpp
+++ menu/control_picker.cpp
@@ -1,19 +1,43 @@
 #include "control_picker.h"
 #include "label.h"
+#include "chooser.h"
 #include "resource_manager.h"
+#include "config.h"
+#include "sdlx/joystick.h"
 
-ControlPicker::ControlPicker(const int w, const int h, const std::string &font, const std::string &label, const std::string &config_key) : _config_key(config_key) {
+ControlPicker::ControlPicker(const int w, const int h, const std::string &font, const std::string &label, const std::string &config_key, const std::string &variant) 
+: _config_key(config_key) {
 	int bw, bh;
 	
-	Label *l = new Label(ResourceManager->loadFont(font, false), label);
+	Label *l = new Label(font, label);
 	l->getSize(bw, bh);
 	add(0, 0, l);
+	
+	if (variant == "split") {
+		_values.push_back("keys-1");
+		_values.push_back("keys-2");
+	} else {
+		_values.push_back("keys");
+	}
+	
+	int n = sdlx::Joystick::getCount();
+	for(int i = 0; i < n; ++i) {
+		_values.push_back(mrt::formatString("joy-%d", i));
+	}
+
+	_controls = new Chooser("medium", _values);
+	
+	reload();
 }
 
 void ControlPicker::save() {
-
+	Config->set(_config_key, _controls->getValue());
 }
 
 void ControlPicker::reload() {
-
+	TRY {
+		std::string cm;
+		Config->get(_config_key, cm, "keys");
+		_controls->set(cm);
+	} CATCH("reload", {})
 }
--- menu/control_picker.h
+++ menu/control_picker.h
@@ -4,13 +4,16 @@
 #include "container.h"
 #include <string>
 
+class Chooser;
 class ControlPicker : public Container {
 public: 
-	ControlPicker(const int w, const int h, const std::string &font, const std::string &label, const std::string &config_key);
+	ControlPicker(const int w, const int h, const std::string &font, const std::string &label, const std::string &config_key, const std::string &variant);
 	void save(); 
 	void reload();
 private: 
 	std::string _config_key;
+	std::vector<std::string> _values;
+	Chooser *_controls;
 };
 
 #endif
