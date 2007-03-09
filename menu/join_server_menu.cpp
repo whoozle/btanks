b10cd3051dc6150a5fedfb1bcffe96a04c946b49
Vladimir Menshakov <whoozle@yandex.ru>
2007-03-09 04:22:59 +0000

    do not show 'scan' if release build. (temporary)
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@2580 5c0bf991-d514-0410-a65f-ce4832e571f3

--- menu/join_server_menu.cpp
+++ menu/join_server_menu.cpp
@@ -29,8 +29,10 @@ JoinServerMenu::JoinServerMenu(MainMenu *parent, const int w, const int h) : _pa
 	add(xp, h - 96, _add);
 	xp += 16 + bw;
 
+#ifndef RELEASE
 	_scan->getSize(bw, bh);
 	add(xp, h - 96, _scan);
+#endif
 	
 	_join->getSize(bw, bh);
 	add(w - 64 - bw, h - 96, _join);
