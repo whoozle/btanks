59f24f77e1340d8f80d04bb569b512be5877acea
Vladimir Menshakov <whoozle@yandex.ru>
2007-03-14 07:46:02 +0000

    added debug into to deserialize
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@2679 5c0bf991-d514-0410-a65f-ce4832e571f3

--- objects/missiles_in_vehicle.cpp
+++ objects/missiles_in_vehicle.cpp
@@ -90,8 +90,10 @@ public:
 		s.get(_object);
 		s.get(_type);
 		s.get(_install_default);
+		LOG_DEBUG(("deserialize. max: %d, max visible: %d, quantity: %d", max_n, max_v, n));
 		update();
 		updatePose();
+		LOG_DEBUG(("update. max: %d, max visible: %d, quantity: %d", max_n, max_v, n));
 	}
 	
 private:
