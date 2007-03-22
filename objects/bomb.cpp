e1e237a9ce098815416b9ad17bd5b6ab84687ae2
Vladimir Menshakov <whoozle@yandex.ru>
2007-03-22 10:46:41 +0000

    added helicopter with bombs
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@2863 5c0bf991-d514-0410-a65f-ce4832e571f3

---
+++ objects/bomb.cpp
@@ -0,0 +1,78 @@
+/* Battle Tanks Game
+ * Copyright (C) 2006-2007 Battle Tanks team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "object.h"
+#include "resource_manager.h"
+#include "config.h"
+
+class Bomb : public Object {
+public:
+	Bomb() : Object("bomb") { piercing = true; pierceable = true; }
+	virtual Object * clone() const;
+	virtual void onSpawn();
+	virtual void calculate(const float dt);
+	virtual void tick(const float dt);
+	virtual void emit(const std::string &event, Object * emitter = NULL);
+
+	virtual void serialize(mrt::Serializator &s) const {
+		Object::serialize(s);
+		s.add(z);
+	}
+	
+	virtual void deserialize(const mrt::Serializator &s) {
+		Object::deserialize(s);
+		s.get(z);
+	}
+
+
+private: 
+	float z;
+};
+
+void Bomb::onSpawn() {
+	play("main", false);
+}
+
+void Bomb::calculate(const float dt) {
+	_velocity.x = 0;
+	_velocity.y = 1;
+}
+
+void Bomb::tick(const float dt) {
+	if (getState().empty())
+		emit("death", this);
+	Object::tick(dt);
+}
+
+void Bomb::emit(const std::string &event, Object * emitter) {
+	if (event == "collision") {
+		if (getStateProgress() >= 0.8) 
+			emit("death", emitter);
+		return; //do not emit addDamage
+	} else if (event == "death") {
+		spawn("cannon-explosion", "cannon-explosion");
+	}
+	Object::emit(event, emitter);
+}
+
+
+Object* Bomb::clone() const  {
+	return new Bomb(*this);
+}
+
+REGISTER_OBJECT("bomb", Bomb, ());
--- objects/heli.cpp
+++ objects/heli.cpp
@@ -22,11 +22,13 @@
 #include "alarm.h"
 #include "config.h"
 #include "mrt/random.h"
+#include "tmx/map.h"
+#include "player_manager.h"
 
 class Heli : public Object {
 public:
 	Heli() : 
-		Object("helicopter"), _reaction(true), _fire(false), _left(false) {
+		Object("helicopter"), _reaction(true), _fire(false), _alt_fire(false), _left(false) {
 		impassability = -1;	
 	}
 	
@@ -40,16 +42,20 @@ public:
 	virtual void serialize(mrt::Serializator &s) const {
 		Object::serialize(s);
 		_reaction.serialize(s);
+		_fire.serialize(s);
+		_alt_fire.serialize(s);
 		s.add(_left);
 	}
 	virtual void deserialize(const mrt::Serializator &s) {
 		Object::deserialize(s);
 		_reaction.deserialize(s);
+		_fire.deserialize(s);
+		_alt_fire.deserialize(s);
 		s.get(_left);
 	}	
 
 private: 
-	Alarm _reaction, _fire;
+	Alarm _reaction, _fire, _alt_fire;
 	bool _left;
 };
 
@@ -57,7 +63,23 @@ void Heli::calculate(const float dt) {
 	if (!_reaction.tick(dt))
 		return;
 		
-	_state.fire  = true;
+	_state.fire = _state.alt_fire = true;
+
+	if (!isDriven() && !PlayerManager->isClient()) { 
+		Way way;
+		v2<int> size = Map->getSize();
+		
+		for(int i = 0; i < 3; ++i) {
+			v2<int> next_target;
+			next_target.x = mrt::random(size.x);
+			next_target.y = mrt::random(size.y);
+			way.push_back(next_target);		
+		}
+		setWay(way);
+	}
+	
+	calculateWayVelocity();
+	updateStateFromVelocity();
 
 	GET_CONFIG_VALUE("objects.helicopter.rotation-time", float, rt, 0.2);
 	limitRotation(dt, rt, true, false);	
@@ -71,6 +93,10 @@ void Heli::tick(const float dt) {
 		spawn("helicopter-bullet", _left?"helicopter-bullet-left":"helicopter-bullet-right", v2<float>::empty, _direction);
 		_left = !_left;
 	}
+	if (_state.alt_fire && _alt_fire.tick(dt)) {
+		_alt_fire.reset();
+		spawn("bomb", "bomb");
+	}
 }
 
 void Heli::onSpawn() {
@@ -80,6 +106,9 @@ void Heli::onSpawn() {
 
 	GET_CONFIG_VALUE("objects.helicopter.fire-rate", float, fr, 0.1);
 	_fire.set(fr);
+
+	GET_CONFIG_VALUE("objects.helicopter.bombing-rate", float, br, 0.2);
+	_alt_fire.set(br);
 
 	play("move", true);
 }
