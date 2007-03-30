96de8a0052946da07f40922a98f889e484b3c8de
Vladimir Menshakov <whoozle@yandex.ru>
2007-03-30 09:22:03 +0000

    added table with highlighting and row/col determination
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@2996 5c0bf991-d514-0410-a65f-ce4832e571f3

--- menu/redefine_keys.cpp
+++ menu/redefine_keys.cpp
@@ -2,19 +2,20 @@
 #include "resource_manager.h"
 #include "sdlx/surface.h"
 #include "sdlx/font.h"
+#include "sdlx/rect.h"
 
-RedefineKeys::RedefineKeys() {
+RedefineKeys::RedefineKeys() : _active_row(-1), _active_col(-1) {
 	_bg_table = ResourceManager->loadSurface("menu/keys_table.png");
 	_font = ResourceManager->loadFont("medium", true);
-	_background.init("menu/background_box_dark.png", _bg_table->getWidth() + 32, _bg_table->getHeight() + 32);
+	_background.init("menu/background_box_dark.png", "menu/highlight_medium.png", _bg_table->getWidth() + 32, _bg_table->getHeight() + 32);
 	
-	_actions.push_back("up");
-	_actions.push_back("down");
-	_actions.push_back("left");
-	_actions.push_back("right");
-	_actions.push_back("fire");
-	_actions.push_back("alt-fire");
-	_actions.push_back("disembark");
+	_actions.push_back(Actions::value_type("up", sdlx::Rect()));
+	_actions.push_back(Actions::value_type("down", sdlx::Rect()));
+	_actions.push_back(Actions::value_type("left", sdlx::Rect()));
+	_actions.push_back(Actions::value_type("right", sdlx::Rect()));
+	_actions.push_back(Actions::value_type("fire", sdlx::Rect()));
+	_actions.push_back(Actions::value_type("alt-fire", sdlx::Rect()));
+	_actions.push_back(Actions::value_type("disembark", sdlx::Rect()));
 }
 
 void RedefineKeys::render(sdlx::Surface &surface, const int x, const int y) {
@@ -24,7 +25,16 @@ void RedefineKeys::render(sdlx::Surface &surface, const int x, const int y) {
 	
 	int yp = y + dy + 50;
 	for(size_t i = 0; i < _actions.size(); ++i) {
-		_font->render(surface, x + 36, yp, _actions[i]);
+		sdlx::Rect &rect = _actions[i].second;
+		rect.x = 0;
+		rect.y = yp - 15 - y;
+		rect.h = _font->getHeight() + 30;
+		rect.w = _background.w;
+		
+		if (_active_row == (int)i) {
+			_background.renderHL(surface, x, yp + _font->getHeight() / 2 + 1);
+		}
+		_font->render(surface, x + 36, yp, _actions[i].first);
 		yp += 30;
 	}
 }
@@ -35,15 +45,38 @@ void RedefineKeys::getSize(int &w, int &h) const {
 }
 
 bool RedefineKeys::onKey(const SDL_keysym sym) {
-//	if (Container::onKey(sym)) 
-//		return true;
 	switch(sym.sym) {
 
 	case SDLK_ESCAPE: 
 		hide(true);
+		return true;
+		
 	default: ;
-
 	}
 	
+	if (_active_row == -1 || _active_col == -1) 
+		return true;
+	
+	LOG_DEBUG(("key: %s", SDL_GetKeyName(sym.sym)));
+		
+	
+	return true;
+}
+
+bool RedefineKeys::onMouseMotion(const int state, const int x, const int y, const int xrel, const int yrel) {
+	_active_col = _active_row = -1;
+	int dx = (_background.w - _bg_table->getWidth()) / 2;
+	for(size_t i = 0; i < _actions.size(); ++i) {
+		const sdlx::Rect &rect = _actions[i].second;
+		if (rect.in(x, y)) 
+			_active_row = i;
+		int col = (x - dx - 148);
+		if (col >= 0) {
+			col /=110;
+			if (col >= 0 && col < 3)
+				_active_col = col;
+			//LOG_DEBUG(("col %d", _active_col));
+		}
+	}
 	return true;
 }
--- menu/redefine_keys.h
+++ menu/redefine_keys.h
@@ -9,6 +9,7 @@
 
 namespace sdlx {
 	class Font;
+	class Rect;
 }
 
 class RedefineKeys : public Control {
@@ -18,13 +19,17 @@ public:
 	virtual void render(sdlx::Surface &surface, const int x, const int y);
 	virtual void getSize(int &w, int &h) const;
 	virtual bool onKey(const SDL_keysym sym);
+	virtual bool onMouseMotion(const int state, const int x, const int y, const int xrel, const int yrel);
 
 private: 
 	const sdlx::Surface *_bg_table;
 	const sdlx::Font * _font;
 	Box _background;
 	
-	std::vector<std::string> _actions;
+	int _active_row, _active_col;
+	
+	typedef std::vector<std::pair<std::string, sdlx::Rect> > Actions; 
+	Actions _actions;
 };
 
 #endif
