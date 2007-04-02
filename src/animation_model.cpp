6c17881c3b74501ce0c3923d81d34ba48221e583
Vladimir Menshakov <whoozle@yandex.ru>
2007-04-02 05:21:58 +0000

    fixed amd64 building warning
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@3004 5c0bf991-d514-0410-a65f-ce4832e571f3

--- src/animation_model.cpp
+++ src/animation_model.cpp
@@ -25,7 +25,7 @@ AnimationModel::AnimationModel(const float default_speed) : default_speed(defaul
 void AnimationModel::addPose(const std::string &id, Pose *pose) {
 	delete _poses[id];
 	_poses[id] = pose;
-	LOG_DEBUG(("pose '%s' with %d frames added (speed: %f)", id.c_str(), pose->frames.size(), pose->speed));
+	LOG_DEBUG(("pose '%s' with %u frames added (speed: %f)", id.c_str(), (unsigned)pose->frames.size(), pose->speed));
 }
 
 const Pose * AnimationModel::getPose(const std::string &id) const {
