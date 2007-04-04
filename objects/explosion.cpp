0b81db4f8ace846e92ab43abf4c4d6be4506becc
Vladimir Menshakov <whoozle@yandex.ru>
2007-04-04 06:47:48 +0000

    do not add damage if just 'explosion'
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@3092 5c0bf991-d514-0410-a65f-ce4832e571f3

--- objects/explosion.cpp
+++ objects/explosion.cpp
@@ -99,7 +99,7 @@ void Explosion::emit(const std::string &event, Object * emitter) {
 			return;
 		
 		//nuke damage.
-		if (emitter == NULL || 
+		if (emitter == NULL || registered_name == "explosion" || 
 			(emitter->registered_name.size() >= 9 && 
 			emitter->registered_name.substr(emitter->registered_name.size() - 9, 9) == "explosion")
 			)
