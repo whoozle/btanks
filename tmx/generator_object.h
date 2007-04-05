2b85f76615ea7234a7f60119fd0179e8703e083f
Vladimir Menshakov <whoozle@yandex.ru>
2007-04-05 11:07:15 +0000

    added base 'render'
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@3134 5c0bf991-d514-0410-a65f-ce4832e571f3

--- tmx/generator_object.h
+++ tmx/generator_object.h
@@ -4,11 +4,14 @@
 #include <string>
 #include <map>
 
+class Layer;
+
 class GeneratorObject {
 public: 
 	int w, h;
 
 	virtual void init(const std::map<const std::string, std::string>& attrs, const std::string &data);
+	virtual void render(Layer *layer, const int first_gid, const int x, const int y) const = 0;
 	virtual ~GeneratorObject() {}
 	
 	static GeneratorObject *create(const std::string &name, const std::map<const std::string, std::string>& attrs, const std::string &data);
