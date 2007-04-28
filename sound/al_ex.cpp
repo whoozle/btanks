ea20724e05ab15568b904ca374c150aed32ee471
Vladimir Menshakov <whoozle@yandex.ru>
2007-04-28 08:41:18 +0000

    added al error reporting
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@3407 5c0bf991-d514-0410-a65f-ce4832e571f3

--- sound/al_ex.cpp
+++ sound/al_ex.cpp
@@ -19,11 +19,11 @@
 
 #include "al_ex.h"
 #include <assert.h>
-#include <AL/alut.h>
+#include <AL/al.h>
 
 ALException::ALException(const ALenum code): _code(code) {}
 const std::string ALException::getCustomMessage() {
-	const char * err = alutGetErrorString(_code);
-	assert(err != NULL);
-	return err;
+	//const char * err = alGetErrorString(_code);
+	//assert(err != NULL);
+	return mrt::formatString("openAL error: %d %08x", (int)_code, (unsigned)_code);
 }
