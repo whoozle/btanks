aa7b10c0d8d4b171df94216633310fdb8ad5b556
Vladimir Menshakov <whoozle@yandex.ru>
2007-05-14 07:51:35 +0000

    implemented checkbox control
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@3561 5c0bf991-d514-0410-a65f-ce4832e571f3

---
+++ menu/checkbox.cpp
@@ -0,0 +1,36 @@
+#include "checkbox.h"
+#include "resource_manager.h"
+#include "sdlx/surface.h"
+
+Checkbox::Checkbox(const bool state) : 
+	_state(state) , _checkbox(ResourceManager->loadSurface("menu/checkbox.png")) {
+}
+
+const bool Checkbox::get() const {
+	return _state;
+}
+
+bool Checkbox::onMouse(const int button, const bool pressed, const int x, const int y) {
+	if (pressed) 
+		return true;
+	_state = !_state;
+	_changed = true;
+	return true;
+}
+
+
+void Checkbox::render(sdlx::Surface &surface, const int x, const int y) {
+	int tw = _checkbox->getWidth() / 2, th = _checkbox->getHeight();
+	if (_state) {
+		sdlx::Rect fg(tw, 0, _checkbox->getWidth() - tw, th);
+		surface.copyFrom(*_checkbox, fg, x, y);
+	} else {
+		sdlx::Rect bg(0, 0, tw, th);
+		surface.copyFrom(*_checkbox, bg, x, y);
+	}
+}
+
+void Checkbox::getSize(int &w, int &h) const {
+	w = _checkbox->getWidth() / 2;
+	h = _checkbox->getHeight();
+}
---
+++ menu/checkbox.h
@@ -0,0 +1,21 @@
+#ifndef BTANKS_MENU_CHECKBOX_H__
+#define BTANKS_MENU_CHECKBOX_H__
+
+#include "control.h"
+
+class BTANKSAPI Checkbox : public Control {
+public: 
+	Checkbox(const bool state = false);
+	const bool get() const;
+
+	virtual void render(sdlx::Surface &surface, const int x, const int y);
+	virtual void getSize(int &w, int &h) const;
+	virtual bool onMouse(const int button, const bool pressed, const int x, const int y);
+	
+private: 
+	bool _state;
+	const sdlx::Surface *_checkbox;
+};
+
+#endif
+
