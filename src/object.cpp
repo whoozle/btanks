429072ed8db886ccbc9c816edad87691c8d270da
Vladimir Menshakov <whoozle@yandex.ru>
2007-05-15 08:41:11 +0000

    fixed one-point way setup
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@3590 5c0bf991-d514-0410-a65f-ce4832e571f3

--- src/object.cpp
+++ src/object.cpp
@@ -591,8 +591,10 @@ void Object::emit(const std::string &event, Object * emitter) {
 void Object::setWay(const Way & way) {
 	_way = way;
 	_next_target.clear();
-	if (!way.empty()) 
+	if (!way.empty()) { 
 		LOG_DEBUG(("%d:%s:%s set %u pending waypoints", getID(), registered_name.c_str(), animation.c_str(), (unsigned)_way.size()));
+		_next_target = way.begin()->convert<float>();
+	}
 }
 
 void Object::calculateWayVelocity() {
