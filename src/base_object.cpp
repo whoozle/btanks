fdf8ea873bd42272b8ac26f0935530363f379d06
Vladimir Menshakov <whoozle@yandex.ru>
2007-05-21 09:46:05 +0000

    skip piercing objects.
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@3668 5c0bf991-d514-0410-a65f-ce4832e571f3

--- src/base_object.cpp
+++ src/base_object.cpp
@@ -287,7 +287,8 @@ const bool BaseObject::hasSameOwner(const BaseObject *other, const bool skip_coo
 	while(i != _owner_set.end() && j != other->_owner_set.end()) {
 		const int l = *i, r = *j;
 		if (l == r) {
-			if (skip_cooperative && l == OWNER_COOPERATIVE) {
+			if (skip_cooperative && l == OWNER_COOPERATIVE && 
+				!piercing && !other->piercing) {
 				++i; ++j;
 				continue;
 			}
