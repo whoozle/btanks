93dae65025389a5280d3a7d7931757a060813c5c
Vladimir Menshakov <whoozle@yandex.ru>
2007-05-31 08:19:56 +0000

    implemented relativePath
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@3858 5c0bf991-d514-0410-a65f-ce4832e571f3

--- mrt/fs_node.cpp
+++ mrt/fs_node.cpp
@@ -8,6 +8,7 @@
 #endif
 
 #include "mrt/exception.h"
+#include <deque>
 
 using namespace mrt;
 
@@ -27,11 +28,40 @@ const std::string FSNode::getDir(const std::string &fname) {
 	return fname.substr(0, p - 1);
 }
 
+static void pack_path(std::deque<std::string> &result, const std::vector<std::string> &path, const size_t start) {
+	result.clear();
+	for(size_t i = start; i < path.size(); ++i) {
+		const std::string &e = path[i];
+		if (e == ".")
+			continue;
+		if (e == ".." && !result.empty())
+			result.pop_back();
+		result.push_back(e);
+	}
+}
 
 const std::string FSNode::relativePath(const std::string &from_dir, const std::string &to_dir) {
 	std::vector<std::string> f_path, t_path;
 	mrt::split(f_path, from_dir, "/");
 	mrt::split(t_path, to_dir, "/");
-	throw_ex(("implement me"));
-	return std::string();
+
+	size_t base = 0;
+	for(; base < f_path.size() && base < t_path.size(); ++base) {
+		if (f_path[base] != t_path[base])
+			break;
+	}
+
+	std::deque<std::string> f, t;
+	pack_path(f, f_path, base);
+	pack_path(t, t_path, base);	
+	
+	std::vector<std::string> result;
+	for(size_t i = 0; i < f.size(); ++i) 
+		result.push_back("..");
+	for(size_t i = 0; i < t.size(); ++i) 
+		result.push_back(t[i]);
+	
+	std::string r_str;
+	join(r_str, result, "/");
+	return r_str;
 }
