5e52c8ea31743c222125a7922453505fcacaec2e
Vladimir Menshakov <whoozle@yandex.ru>
2007-06-09 04:20:36 +0000

    compile crash handler in non-win32 mode
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@3983 5c0bf991-d514-0410-a65f-ce4832e571f3

--- mrt/crash.cpp
+++ mrt/crash.cpp
@@ -6,12 +6,13 @@
 #	include <unistd.h>
 #	include <stdlib.h>
 #	include <stdio.h>
-#endif
 
 static void crash_handler(int sno) {
 	fprintf(stdout, "btanks crashed with signal %d. use gdb -p %d to debug it. zzZzzZZzz...\n\n", sno, getpid());
 	sleep(3600);
 }
+
+#endif
 
 void mrt::install_crash_handlers() {
 #ifndef WIN32
