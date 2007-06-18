c729daf8acba0b8eaa1a0a458623952eb60053bd
Vladimir Menshakov <whoozle@yandex.ru>
2007-06-18 09:48:26 +0000

    skip objects with disabled ai too
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@4071 5c0bf991-d514-0410-a65f-ce4832e571f3

--- src/object.cpp
+++ src/object.cpp
@@ -915,7 +915,7 @@ const int Object::getTargetPosition(v2<float> &relative_position, const std::set
 		dir.fromDirection(d, dirs);
 		for(std::set<const Object *>::const_iterator i = objects.begin(); i != objects.end(); ++i) {
 			const Object *o = *i;
-			if (hasSameOwner(o))
+			if (hasSameOwner(o) || o->aiDisabled())
 				continue;
 			
 			v2<float> pos, tp = getRelativePosition(o);
