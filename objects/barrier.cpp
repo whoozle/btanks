0b4f7022e5657b92ef7aa9e2cee21502112cd4f6
Vladimir Menshakov <whoozle@yandex.ru>
2007-06-21 10:31:39 +0000

    added barrier code
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@4114 5c0bf991-d514-0410-a65f-ce4832e571f3

---
+++ objects/barrier.cpp
@@ -0,0 +1,90 @@
+/* Battle Tanks Game
+ * Copyright (C) 2006-2007 Battle Tanks team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "object.h"
+#include "resource_manager.h"
+#include "config.h"
+#include "alarm.h"
+
+class Barrier : public Object {
+public:
+	Barrier() : Object("barrier"), _toggle(true) { pierceable = true; }
+	virtual Object * clone() const;
+	virtual void onSpawn();
+	virtual void calculate(const float dt);
+	virtual void tick(const float dt);
+	virtual void emit(const std::string &event, Object * emitter = NULL);
+
+	virtual void serialize(mrt::Serializator &s) const {
+		Object::serialize(s);
+		s.add(_toggle);
+	}
+
+	virtual void deserialize(const mrt::Serializator &s) {
+		Object::deserialize(s);
+		s.get(_toggle);
+	}
+
+private: 
+	Alarm _toggle;
+};
+
+void Barrier::onSpawn() {
+	GET_CONFIG_VALUE("objects.barrier.toggle-interval", float, ti, 3.0f);
+	_toggle.set(ti);
+	play("closed", true);
+}
+
+void Barrier::calculate(const float dt) {
+	if (_toggle.tick(dt))
+		_state.fire = !_state.fire;
+}
+
+void Barrier::tick(const float dt) {
+	Object::tick(dt);
+	if (_state.fire && getState() == "closed") {
+		cancelAll();
+		play("opening", false);
+		play("opened", true);
+	}
+
+	if (!_state.fire && getState() == "opened") {
+		cancelAll();
+		play("closing", false);
+		play("closed", true);
+	}
+}
+
+void Barrier::emit(const std::string &event, Object * emitter) {
+	if (event == "collision") {
+		if (emitter == NULL || getStateProgress() >= 0.8) 
+			emit("death", emitter);
+		return; //do not emit addDamage
+	} else if (event == "death") {
+		Object *o = spawn("cannon-explosion", "cannon-explosion");
+		o->setZ(getZ() + 1, true);
+	}
+	Object::emit(event, emitter);
+}
+
+
+Object* Barrier::clone() const  {
+	return new Barrier(*this);
+}
+
+REGISTER_OBJECT("barrier", Barrier, ());
