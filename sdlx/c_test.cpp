5bb0bb9c1bea06331ce0015895636c7f72931661
Vladimir Menshakov <whoozle@yandex.ru>
2007-06-21 05:43:59 +0000

    updated test
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@4106 5c0bf991-d514-0410-a65f-ce4832e571f3

--- sdlx/c_test.cpp
+++ sdlx/c_test.cpp
@@ -4,25 +4,13 @@
 
 int main() {
 	sdlx::CollisionMap map1, map2;
-	map1.create(15, 15, 1);
+	map1.create(7, 7, 1);
 	LOG_DEBUG(("map dump: %s", map1.dump().c_str()));
-	map2.create(15, 15, 1);
-	sdlx::Rect pixel(7, 7, 1, 1);
-	for(int y = 0; y < 15; ++y) 
-		for(int x = 0; x < 15; ++x) {
-			bool b = map1.collides(pixel, &map2, pixel, x, y, false);
+	map2.create(7, 7, 1);
+	for(int y = -8; y < 8; ++y) 
+		for(int x = -8; x < 8; ++x) {
+			sdlx::Rect pixel1(0, 0, 1, 1), pixel2(3, 3, 1, 1);
+			bool b = map1.collides(pixel1, &map2, pixel2, x, y, false);
 			LOG_DEBUG(("%d,%d: %s", x, y, b?"true":"false"));
 		}
-	LOG_DEBUG(("worst test"));
-
-	for(int y = 0; y < 15; ++y) {
-		sdlx::Rect r(14, 14, 1, 1);
-		bool b = map1.collides(r, &map2, r, 0, y, false);
-		LOG_DEBUG(("x=0, 0,%d: %s", y, b?"true":"false"));
-	}
-	for(int x = 0; x < 15; ++x) {
-		sdlx::Rect r(14, 14, 1, 1);
-		bool b = map1.collides(r, &map2, r, x, 0, false);
-		LOG_DEBUG(("y=0, %d,0: %s", x, b?"true":"false"));
-	}
 }
