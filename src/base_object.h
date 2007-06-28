01c011e49e27d674ef763b3e5d942e877e743a6b
Vladimir Menshakov <whoozle@yandex.ru>
2007-06-28 09:35:51 +0000

    added getVariants()
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@4210 5c0bf991-d514-0410-a65f-ce4832e571f3

--- src/base_object.h
+++ src/base_object.h
@@ -115,6 +115,8 @@ public:
 
 	const float getEffectiveImpassability(const float impassability) const;
 	
+	const Variants & getVariants() const { return _variants; }
+	
 protected:
 	int _id;
 	int _follow;
