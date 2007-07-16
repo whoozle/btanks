7191e69225f5ced473fba9adee189cfb60564998
Vladimir Menshakov <whoozle@yandex.ru>
2007-07-16 16:04:56 +0000

    initialize size
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@4426 5c0bf991-d514-0410-a65f-ce4832e571f3

--- mrt/utf8_utils.cpp
+++ mrt/utf8_utils.cpp
@@ -1,7 +1,7 @@
 #include "utf8_utils.h"
 
 const std::string::size_type mrt::utf8_length(const std::string &str) {
-	std::string::size_type size;
+	std::string::size_type size = 0;
 	for(std::string::size_type p = 0; p < str.size(); ++p) {
 		std::string::value_type c = str[p];
 		if ((c & 0x80) == 0 || (c & 0xc0) != 0x80)
