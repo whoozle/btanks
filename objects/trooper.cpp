087e9019708559a6fd61c16dc9fac07defec76fc
Vladimir Menshakov <whoozle@yandex.ru>
2007-07-20 06:35:05 +0000

    slightly cleaned up penalty func
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@4497 5c0bf991-d514-0410-a65f-ce4832e571f3

--- objects/trooper.cpp
+++ objects/trooper.cpp
@@ -24,9 +24,9 @@
 #include "trooper.h"
 
 void Trooper::getImpassabilityPenalty(const float impassability, float &base, float &base_value, float &penalty) const {
-	if (impassability > 0.2) {
+	if (impassability > 0.2f) {
 		base_value = 0.2f;
-		base = 0.2f;
+		base = 0;
 		penalty = 0;
 	}
 }
