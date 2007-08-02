9ff221f81425da879fa77f77da4d181500a11cb5
Vladimir Menshakov <whoozle@yandex.ru>
2007-08-02 10:40:48 +0000

    display actual animation instead of player-slot
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@4599 5c0bf991-d514-0410-a65f-ce4832e571f3

--- src/hud.cpp
+++ src/hud.cpp
@@ -136,7 +136,8 @@ void Hud::renderStats(sdlx::Surface &surface) {
 		if (slot.id == -1)
 			continue;
 		surface.fillRect(sdlx::Rect(xp, yp, box_w1, box_h), index2color(surface, p + 1, 255));
-		_font->render(surface, xp + box_w2, yp, mrt::formatString("%s (%s)", slot.name.c_str(), slot.animation.c_str()));
+		const Object * o = slot.getObject();
+		_font->render(surface, xp + box_w2, yp, mrt::formatString("%s (%s)", slot.name.c_str(), o? o->animation.c_str():"-"));
 		std::string score = mrt::formatString("%d", slot.frags);
 		int sw = _font->render(NULL, 0, 0, score);
 		_font->render(surface, xp + background.w - 2 * mx - sw, yp, score);
