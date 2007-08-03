5eea06b3443d1292b3d409af2cdaee9a3e7ffaba
Vladimir Menshakov <whoozle@yandex.ru>
2007-08-03 08:57:38 +0000

    do not render hp bars for invulnerable objects
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@4624 5c0bf991-d514-0410-a65f-ce4832e571f3

--- src/world.cpp
+++ src/world.cpp
@@ -252,7 +252,7 @@ void IWorld::render(sdlx::Surface &surface, const sdlx::Rect& src, const sdlx::R
 					wp.x - src.x + dst.x - 8, wp.y - src.y + dst.y - 8);
 			}
 		}
-		if (special_ids.find(o.getID()) != special_ids.end() || (rhb && (o.impassability == 1.0f && o._follow <= 0 && !o.piercing))) {
+		if (o.hp >= 0 && special_ids.find(o.getID()) != special_ids.end() || (rhb && (o.impassability == 1.0f && o._follow <= 0 && !o.piercing))) {
 			int h = _hp_bar->getHeight() / 16;
 			int y = (o.hp >= 0)?15 * (o.max_hp - o.hp) / o.max_hp: 0;
 			sdlx::Rect hp_src(0, y * h, _hp_bar->getWidth(), h);
