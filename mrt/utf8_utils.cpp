4c0c83274c7b3e039c09157ebbf4d7d87a8db29f
Vladimir Menshakov <whoozle@yandex.ru>
2007-08-03 06:01:42 +0000

    added utf8 left/right/backspace
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@4613 5c0bf991-d514-0410-a65f-ce4832e571f3

--- mrt/utf8_utils.cpp
+++ mrt/utf8_utils.cpp
@@ -9,3 +9,54 @@ const std::string::size_type mrt::utf8_length(const std::string &str) {
 	}
 	return size;
 }
+
+void mrt::utf8_add_wchar(std::string &str, const int wchar) {
+	if (wchar <= 0x7f) {
+		str += (char)wchar;
+	} else if (wchar <= 0x7ff) {
+		str += (char) ((wchar >> 6) | 0xc0);
+		str += (char) ((wchar & 0x3f) | 0x80);
+	} else if (wchar <= 0xffff) {
+		str += (char)((wchar >> 12) | 0xe0);
+		str += (char)(((wchar & 0x0fc0) >> 6) | 0x80);
+		str += (char)( (wchar & 0x003f) | 0x80);
+	} else if (wchar <= 0x10ffff) {
+		str += (char)((wchar >> 18) | 0xf0);
+		str += (char)(((wchar & 0x03f000) >> 12) | 0x80);
+		str += (char)(((wchar & 0x000fc0) >> 6) | 0x80);
+		str += (char)( (wchar & 0x00003f) | 0x80);
+	} else 
+		str += '?';
+}
+
+void mrt::utf8_backspace(std::string &str, size_t pos) {
+	if (str.empty())
+		return;
+	if (pos >= str.size())
+		pos = str.size() - 1;
+
+	int p;
+	for(p = (int)str.size() - 1; p >= 0 && (str[p] & 0xc0) == 0x80; --p) {}
+	if (p > 0)
+		str.resize(p);
+	else 
+		str.clear(); //p <= 0
+}
+
+const size_t mrt::utf8_left(const std::string &str, const size_t pos) {
+	if (pos == 0 || str.empty())
+		return 0;
+
+	int p;
+	for(p = (int)pos - 1; p >= 0 && (str[p] & 0xc0) == 0x80; --p) {}
+	return (p > 0)? p: 0;	
+}
+
+const size_t mrt::utf8_right(const std::string &str, const size_t pos) {
+	if (str.empty())
+		return 0;
+
+	size_t p;
+	for(p = pos + 1; p < str.size() && (str[p] & 0xc0) == 0x80; ++p) {}
+	return p >= str.size()? str.size() - 1: p;
+}
