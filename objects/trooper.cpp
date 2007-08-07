445757b1c46ae9ba3375bd1abd1ba267c2fa830a
Vladimir Menshakov <whoozle@yandex.ru>
2007-08-07 07:02:21 +0000

    removed hardcoded names for civilian corpses
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@4640 5c0bf991-d514-0410-a65f-ce4832e571f3

--- objects/trooper.cpp
+++ objects/trooper.cpp
@@ -81,7 +81,7 @@ void Trooper::onSpawn() {
 
 void Trooper::emit(const std::string &event, Object * emitter) {
 	if (event == "death") {
-		spawn("corpse(human-death)", (registered_name.substr(0, 8) == "civilian")? "dead-civilian-1": "dead-machinegunner", v2<float>(), v2<float>());
+		spawn("corpse(human-death)", "dead-" + registered_name, v2<float>(), v2<float>());
 	} else if (event == "collision" && emitter != NULL && emitter->classname == "vehicle") {
 		if (_velocity.same_sign(getRelativePosition(emitter)) &&
 			attachVehicle(emitter))
