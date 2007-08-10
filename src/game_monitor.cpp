eb7fec95dc5f09de3a621e06607c73feb9bf37a8
Vladimir Menshakov <whoozle@yandex.ru>
2007-08-10 11:26:26 +0000

    added timer limit handling
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@4696 5c0bf991-d514-0410-a65f-ce4832e571f3

--- src/game_monitor.cpp
+++ src/game_monitor.cpp
@@ -672,6 +672,13 @@ void IGameMonitor::loadMap(Campaign *campaign, const std::string &name, const bo
 		GameMonitor->killAllClasses(classes);
 		LOG_DEBUG(("kill'em all classes: %u", (unsigned)classes.size()));
 	}
+
+	if (Config->has("multiplayer.time-limit")) {
+		int tl; 
+		Config->get("multiplayer.time-limit", tl, 0);
+		if (tl != 0) 
+			setTimer("messages", "time-limit-reached", (float)tl, false);
+	}
 	
 	LOG_DEBUG(("generating matrixes"));
 	Map->generateMatrixes();
