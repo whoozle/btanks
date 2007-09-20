af380fef3e3befcb23ab453c9d62bc600c7e73eb
Vladimir Menshakov <whoozle@yandex.ru>
2007-09-20 02:57:43 +0000

    rewrited zlib handling
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@5027 5c0bf991-d514-0410-a65f-ce4832e571f3

--- mrt/gzip.cpp
+++ mrt/gzip.cpp
@@ -25,7 +25,7 @@ using namespace mrt;
 
 #define throw_z(method, ret) throw_ex(("zlib.%s failed: %s, code: %d", method, z.msg, ret));
 
-#define BUF 0x10000
+#define BUF 0x1000
 
 void ZStream::decompress(mrt::Chunk &dst, const mrt::Chunk &src, const bool gzip_header) {
 	z_stream z;
@@ -39,29 +39,21 @@ void ZStream::decompress(mrt::Chunk &dst, const mrt::Chunk &src, const bool gzip
 			throw_z("inflateInit", ret);
 		
 		dst.setSize(BUF);
-		size_t out_pos = 0;
-
-		z.avail_out = dst.getSize();
-		z.next_out = (Bytef*)dst.getPtr() + out_pos;
+		do { 
+			if (z.avail_in == 0)
+				break;
+			z.avail_out = dst.getSize() - z.total_out;
+			z.next_out = (Bytef*)dst.getPtr() + z.total_out;
 		
-		do {
-			//LOG_DEBUG(("inflate"));
-			if (z.avail_in ==0)
-				break; //fixme ?
-			
-			ret = inflate(&z, Z_NO_FLUSH);
+			ret = inflate(&z, Z_FINISH);
 			
 			if (ret == Z_STREAM_END) 
 				break;
 			
-			if (ret == Z_BUF_ERROR) {
+			if (ret == Z_BUF_ERROR || z.avail_out == 0) {
 				if (z.avail_out == 0) {
 					LOG_DEBUG(("ran out of out buf"));
-					dst.setSize(dst.getSize() + BUF);
-					
-					out_pos += BUF;
-					z.next_out = (Bytef*)dst.getPtr() + out_pos;
-					z.avail_out = BUF;
+					dst.setSize(dst.getSize() + BUF);					
 					continue;
 				} else if (z.avail_in == 0) {
 					throw_ex(("stream was truncated. unable to proceed."));
@@ -70,8 +62,9 @@ void ZStream::decompress(mrt::Chunk &dst, const mrt::Chunk &src, const bool gzip
 
 			if (ret != Z_OK)
 				throw_z("inflate", ret);
-			
-		} while(1);
+		
+		} while(true);
+
 		if ((ret = inflateEnd(&z)) != Z_OK) 
 			throw_z("inflateEnd", ret);
 		
@@ -91,29 +84,20 @@ void ZStream::compress(mrt::Chunk &dst, const mrt::Chunk &src, const bool gzip_h
 			throw_z("DeflateInit", ret);
 		
 		dst.setSize(BUF);
-		size_t out_pos = 0;
 
-		z.avail_out = dst.getSize();
-		z.next_out = (Bytef*)dst.getPtr() + out_pos;
-		
 		do {
-			//LOG_DEBUG(("deflate"));
-			if (z.avail_in ==0)
-				break; //fixme ?
-			
+			z.avail_out = dst.getSize() - z.total_out;
+			z.next_out = (Bytef*)dst.getPtr() + z.total_out;
+		
 			ret = deflate(&z, Z_FINISH);
 			
 			if (ret == Z_STREAM_END) 
 				break;
 			
-			if (ret == Z_BUF_ERROR) {
+			if (ret == Z_BUF_ERROR || z.avail_out == 0) {
 				if (z.avail_out == 0) {
 					LOG_DEBUG(("ran out of out buf"));
-					dst.setSize(dst.getSize() + BUF);
-					
-					out_pos += BUF;
-					z.next_out = (Bytef*)dst.getPtr() + out_pos;
-					z.avail_out = BUF;
+					dst.setSize(dst.getSize() + BUF);					
 					continue;
 				} else if (z.avail_in == 0) {
 					throw_ex(("stream was truncated. unable to proceed."));
@@ -123,7 +107,7 @@ void ZStream::compress(mrt::Chunk &dst, const mrt::Chunk &src, const bool gzip_h
 			if (ret != Z_OK)
 				throw_z("deflate", ret);
 			
-		} while(1);
+		} while(true);
 		if ((ret = deflateEnd(&z)) != Z_OK) 
 			throw_z("deflateEnd", ret);
 		
