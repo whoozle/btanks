5e25d6213057eb66f6c6120a26b9b9230467aed4
Vladimir Menshakov <whoozle@yandex.ru>
2007-10-15 10:49:10 +0000

    added spawn() function for lua
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@5164 5c0bf991-d514-0410-a65f-ce4832e571f3

--- luaxx/lua_hooks.cpp
+++ luaxx/lua_hooks.cpp
@@ -1,6 +1,11 @@
 #include "lua_hooks.h"
+#include "special_owners.h"
 #include "mrt/logger.h"
+#include "object.h"
+#include "world.h"
+#include "resource_manager.h"
 #include <assert.h>
+#include <stdexcept>
 
 static int lua_hooks_print(lua_State *L) {
 	int n = lua_gettop(L);
@@ -14,11 +19,47 @@ static int lua_hooks_print(lua_State *L) {
 	return 0;
 }
 
+static int lua_hooks_spawn(lua_State *L) {
+	int n = lua_gettop(L);
+	if (n < 4) {
+		lua_pushstring(L, "spawn() requires at least 4 arguments: classname, animation");
+		lua_error(L);
+	}
+	try {
+		const char *classname = lua_tostring(L, 1);
+		if (classname == NULL)
+			throw std::runtime_error("spawn: first argument must be string");
+		const char *animation = lua_tostring(L, 2);
+		if (animation == NULL)
+			throw std::runtime_error("spawn: second argument must be string");
+		int x = lua_tointeger(L, 3);
+		int y = lua_tointeger(L, 4);
+		
+		Object *o = ResourceManager->createObject(classname, animation);
+	//	if (z) 
+	//		o->setZ(z, true);
+		o->addOwner(OWNER_MAP);
+
+	//	if (dir) 
+	//		o->setDirection(dir);
+	
+		World->addObject(o, v2<float>(x, y));
+	} catch(const std::exception &e) {
+		lua_pushstring(L, e.what());
+		lua_error(L);
+	} catch(...) {
+		lua_pushstring(L, "unknown exception");
+		lua_error(L);
+	}
+	return 0;
+}
+
 void LuaHooks::load(const std::string &name) {
 	LOG_DEBUG(("loading lua code from %s...", name.c_str()));
 	state.loadFile(name);
 	
 	lua_register(state, "print", lua_hooks_print);
+	lua_register(state, "spawn", lua_hooks_spawn);
 	
 	state.call(0, LUA_MULTRET);
 	
