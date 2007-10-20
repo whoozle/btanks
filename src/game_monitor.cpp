120b254cf27b24e3c107c9ab0e83e0239d2a9829
Vladimir Menshakov <whoozle@yandex.ru>
2007-10-20 14:10:19 +0000

    removed lua_hooks initialization
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@5202 5c0bf991-d514-0410-a65f-ce4832e571f3

--- src/game_monitor.cpp
+++ src/game_monitor.cpp
@@ -51,8 +51,6 @@ IMPLEMENT_SINGLETON(GameMonitor, IGameMonitor);
 IGameMonitor::IGameMonitor() : _game_over(false), _win(false), _check_items(0.5, true), _state_timer(false), _timer(0), _campaign(NULL)
 #ifdef ENABLE_LUA
 , lua_hooks(new LuaHooks) 
-#else
-, lua_hooks(NULL)
 #endif
 {}
 
