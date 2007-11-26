4fe774b74e2902d0331d723898a16c2ecce5a982
Vladimir Menshakov <whoozle@yandex.ru>
2007-11-26 13:00:10 +0000

    removed skipping of the pierceable objects
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@5523 5c0bf991-d514-0410-a65f-ce4832e571f3

--- objects/explosion.cpp
+++ objects/explosion.cpp
@@ -98,9 +98,6 @@ void Explosion::onSpawn() {
 
 void Explosion::emit(const std::string &event, Object * emitter) {
 	if (event == "collision") {
-		if (emitter == NULL || emitter->pierceable)
-			return;
-		
 		if (emitter == NULL || registered_name == "explosion" || 
 			(emitter->registered_name.size() >= 9 && 
 			emitter->registered_name.substr(emitter->registered_name.size() - 9, 9) == "explosion")
