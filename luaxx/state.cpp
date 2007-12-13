6ed398d39faac5fbffa1a12e873d4e25d4693298
Vladimir Menshakov <whoozle@yandex.ru>
2007-12-13 14:49:52 +0000

    added couple of standard libraries
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@5614 5c0bf991-d514-0410-a65f-ce4832e571f3

--- luaxx/state.cpp
+++ luaxx/state.cpp
@@ -1,10 +1,11 @@
 #include "luaxx/state.h"
 #include "mrt/exception.h"
 #include <stdlib.h>
-#include <lauxlib.h>
+//#include <lauxlib.h>
+#include <assert.h>
 
 using namespace luaxx;
-
+/*
 static void *l_alloc (void *ud, void *ptr, size_t osize, size_t nsize) {
 	if (nsize == 0) {
 		free(ptr);
@@ -13,6 +14,7 @@ static void *l_alloc (void *ud, void *ptr, size_t osize, size_t nsize) {
 		 return realloc(ptr, nsize);
 	}
 }
+*/
 
 static void check_error(lua_State * state, const int err) {
 	switch(err) {
@@ -45,22 +47,48 @@ void State::call(const int nargs, const int nresults) const {
 	check_error(state, err);
 }
 
-State::State() {
-	state = lua_newstate(l_alloc, this);
+void State::init() {
+	assert(state == NULL);
+
+	//state = lua_newstate(l_alloc, this);
+	state = lua_open();
 	if (state == NULL)
 		throw_ex(("cannot create lua interpreter"));
+
+	static const luaL_Reg libs[] = {
+		//{"", luaopen_base},
+		//{LUA_LOADLIBNAME, luaopen_package},
+		{LUA_TABLIBNAME, luaopen_table},
+		//{LUA_IOLIBNAME, luaopen_io},
+		//{LUA_OSLIBNAME, luaopen_os},
+		{LUA_STRLIBNAME, luaopen_string},
+		{LUA_MATHLIBNAME, luaopen_math},
+		 //  {LUA_DBLIBNAME, luaopen_debug},
+		{NULL, NULL}
+	};
+	
+	for (const luaL_Reg *lib = libs; lib->func; ++lib) {
+		lua_pushcfunction(state, lib->func);
+		lua_pushstring(state, lib->name);
+		int err = lua_pcall(state, 1, 0, 0);
+		check_error(state, err);
+	}	
+}
+
+State::State() : state(NULL) {
+	init();
 }
 
 void State::clear() {
 	lua_close(state);
 
-	state = lua_newstate(l_alloc, this);
-	if (state == NULL)
-		throw_ex(("cannot create lua interpreter"));
+	state = NULL;
+	init();
 }
 
 State::~State() {
-	lua_close(state);
+	if (state != NULL)
+		lua_close(state);
 }
 
 
--- luaxx/state.h
+++ luaxx/state.h
@@ -10,13 +10,15 @@ class State {
 public: 
 	State();
 	void loadFile(const std::string &fname);
-	void clear();
+	void open();
 	void call(const int nargs, const int nresults) const;
 	~State();
 	
 	inline operator lua_State*() { return state; } 
+	void clear();
 
 private: 
+	void init();
 	lua_State * state;
 };
 }
