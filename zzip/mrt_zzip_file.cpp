0e3089ee8a27ce7d023e4a9a2588cc20fba61c28
Vladimir Menshakov <whoozle@yandex.ru>
2008-01-09 12:01:23 +0000

    actually implement stub methods
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@5936 5c0bf991-d514-0410-a65f-ce4832e571f3

--- zzip/mrt_zzip_file.cpp
+++ zzip/mrt_zzip_file.cpp
@@ -1,3 +1,42 @@
 #include "mrt_zzip_file.h"
 
 using namespace zzip;
+
+File::File() {}
+
+const bool File::readLine(std::string &str, const size_t bufsize) const {
+	return false;
+}
+
+void File::open(const std::string &fname, const std::string &mode) {
+}
+
+const bool File::opened() const {
+	return false;
+}
+
+int File::seek(long offset, int whence) const {
+	return -1;
+}
+
+long File::tell() const {
+	return -1;
+}
+
+void File::write(const mrt::Chunk &ch) const {
+}
+
+const off_t File::getSize() const {
+	return (off_t)-1;
+}
+const size_t File::read(void *buf, const size_t size) const {
+	return (size_t)-1;
+} 
+void File::close() {
+}
+	
+const bool File::eof() const{
+	return true;
+}
+
+
