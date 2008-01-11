3e4a182aac5217c2a2537f61d3b684fae2e18641
Vladimir Menshakov <whoozle@yandex.ru>
2008-01-11 07:41:28 +0000

    removed base sorting
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@6019 5c0bf991-d514-0410-a65f-ce4832e571f3

--- menu/map_desc.cpp
+++ menu/map_desc.cpp
@@ -19,7 +19,7 @@
 #include "map_desc.h"
 
 const bool MapDesc::operator<(const MapDesc & other) const {
-	if (base != other.base)
-		return base < other.base;
+//	if (base != other.base)
+//		return base < other.base;
 	return name < other.name;	
 } 
