1390a6b7e13a9bdf424e3b6835c8be4aedbe7a79
Vladimir Menshakov <whoozle@yandex.ru>
2008-01-25 12:47:28 +0000

    added disabling of the accesibility option
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@6171 5c0bf991-d514-0410-a65f-ce4832e571f3

--- src/window.cpp
+++ src/window.cpp
@@ -32,6 +32,62 @@
 
 #ifdef _WINDOWS
 #	define putenv _putenv
+
+static STICKYKEYS g_StartupStickyKeys = {sizeof(STICKYKEYS), 0};
+static TOGGLEKEYS g_StartupToggleKeys = {sizeof(TOGGLEKEYS), 0};
+static FILTERKEYS g_StartupFilterKeys = {sizeof(FILTERKEYS), 0};    
+
+void AllowAccessibilityShortcutKeys( bool bAllowKeys )
+{
+    if( bAllowKeys )
+    {
+        // Restore StickyKeys/etc to original state and enable Windows key      
+        STICKYKEYS sk = g_StartupStickyKeys;
+        TOGGLEKEYS tk = g_StartupToggleKeys;
+        FILTERKEYS fk = g_StartupFilterKeys;
+        
+        SystemParametersInfo(SPI_SETSTICKYKEYS, sizeof(STICKYKEYS), &g_StartupStickyKeys, 0);
+        SystemParametersInfo(SPI_SETTOGGLEKEYS, sizeof(TOGGLEKEYS), &g_StartupToggleKeys, 0);
+        SystemParametersInfo(SPI_SETFILTERKEYS, sizeof(FILTERKEYS), &g_StartupFilterKeys, 0);
+    }
+    else
+    {
+        // Disable StickyKeys/etc shortcuts but if the accessibility feature is on, 
+        // then leave the settings alone as its probably being usefully used
+ 
+        STICKYKEYS skOff = g_StartupStickyKeys;
+        if( (skOff.dwFlags & SKF_STICKYKEYSON) == 0 )
+        {
+            // Disable the hotkey and the confirmation
+            skOff.dwFlags &= ~SKF_HOTKEYACTIVE;
+            skOff.dwFlags &= ~SKF_CONFIRMHOTKEY;
+ 
+            SystemParametersInfo(SPI_SETSTICKYKEYS, sizeof(STICKYKEYS), &skOff, 0);
+        }
+ 
+        TOGGLEKEYS tkOff = g_StartupToggleKeys;
+        if( (tkOff.dwFlags & TKF_TOGGLEKEYSON) == 0 )
+        {
+            // Disable the hotkey and the confirmation
+            tkOff.dwFlags &= ~TKF_HOTKEYACTIVE;
+            tkOff.dwFlags &= ~TKF_CONFIRMHOTKEY;
+ 
+            SystemParametersInfo(SPI_SETTOGGLEKEYS, sizeof(TOGGLEKEYS), &tkOff, 0);
+        }
+ 
+        FILTERKEYS fkOff = g_StartupFilterKeys;
+        if( (fkOff.dwFlags & FKF_FILTERKEYSON) == 0 )
+        {
+            // Disable the hotkey and the confirmation
+            fkOff.dwFlags &= ~FKF_HOTKEYACTIVE;
+            fkOff.dwFlags &= ~FKF_CONFIRMHOTKEY;
+ 
+            SystemParametersInfo(SPI_SETFILTERKEYS, sizeof(FILTERKEYS), &fkOff, 0);
+        }
+    }
+}
+
+
 #endif
 
 
@@ -236,8 +292,14 @@ void IWindow::init(const int argc, char *argv[]) {
 		LOG_WARN(("could not find accelerated GL, falling back to software mode"));
 		_opengl = false;
 	}
-
 	createMainWindow();
+#ifdef _WINDOWS
+	SystemParametersInfo(SPI_GETSTICKYKEYS, sizeof(STICKYKEYS), &g_StartupStickyKeys, 0);
+	SystemParametersInfo(SPI_GETTOGGLEKEYS, sizeof(TOGGLEKEYS), &g_StartupToggleKeys, 0);
+	SystemParametersInfo(SPI_GETFILTERKEYS, sizeof(FILTERKEYS), &g_StartupFilterKeys, 0);
+	
+	AllowAccessibilityShortcutKeys(false);
+#endif
 }
 
 void IWindow::createMainWindow() {
@@ -414,6 +476,9 @@ void IWindow::run() {
 }
 
 void IWindow::deinit() {
+#ifdef _WINDOWS
+	AllowAccessibilityShortcutKeys(true);
+#endif
 	_running = false;
 	LOG_DEBUG(("shutting down, freeing surface"));
 	_window.free();
