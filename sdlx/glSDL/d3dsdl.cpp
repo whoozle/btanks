300a38eed150c2a8cbd303f3383991bc2b85ab27
Vladimir Menshakov <whoozle@yandex.ru>
2008-01-25 09:38:02 +0000

    removed unnecessary lockin
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@6165 5c0bf991-d514-0410-a65f-ce4832e571f3

--- sdlx/glSDL/d3dsdl.cpp
+++ sdlx/glSDL/d3dsdl.cpp
@@ -727,21 +727,22 @@ int d3dSDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect,
 		//LOG_DEBUG(("blitting to surfaces"));
 		texinfo * dst_tex = getTexture(dst);
 		if (true || tex == NULL || dst_tex == NULL) {
-			LOG_DEBUG(("generic mixed blit used."));
-			if (src->pixels == NULL) {
+			if (tex != NULL && src->pixels == NULL) {
 				if (d3dSDL_LockSurface2(src) == -1) {
 					return -1;
 				}
 			}
-			if (dst->pixels == NULL) {
+			if (dst_tex != NULL && dst->pixels == NULL) {
 				if (d3dSDL_LockSurface2(dst) == -1) {
 					d3dSDL_UnlockSurface2(src);
 					return -1;
 				}
 			}
 			int r = SDL_BlitSurface(src, srcrect, dst, dstrect);
-			d3dSDL_UnlockSurface2(dst); 
-			d3dSDL_UnlockSurface2(src); 
+			if (dst_tex != NULL)
+				d3dSDL_UnlockSurface2(dst); 
+			if (tex != NULL)
+				d3dSDL_UnlockSurface2(src); 
 			return r;
 		}
 		//both textures are hardware: 
