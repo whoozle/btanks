91082348d908517ce28eca3b95e0d5b80e8a7d25
Vladimir Menshakov <whoozle@yandex.ru>
2008-02-07 09:49:41 +0000

    removed noisy debug, added scaling to the screenshot size
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@6236 5c0bf991-d514-0410-a65f-ce4832e571f3

--- engine/menu/video_control.cpp
+++ engine/menu/video_control.cpp
@@ -10,7 +10,7 @@
 std::map<const SDL_Surface *, VideoControl *> video_controls;
 
 static void update(SDL_Surface* dst, int x, int y, unsigned int w, unsigned int h) {
-	LOG_DEBUG(("%p: update %d, %d, %d, %d", (void *)dst, x, y, w, h));
+	///LOG_DEBUG(("%p: update %d, %d, %d, %d", (void *)dst, x, y, w, h));
 	VideoControl *vc = video_controls[dst];
 	if (vc == NULL)
 		return;
@@ -35,19 +35,19 @@ base(base), name(name), mpeg(0), lock(SDL_CreateMutex()), active(false), updated
 	
 	fname = "maps/" + name + ".mpg";
 	if (Finder->exists(base, fname)) {
-		mrt::Chunk video_data;
-		Finder->load(video_data, fname);
-		LOG_DEBUG(("video file loaded (%u bytes)", (unsigned)video_data.getSize()));
+		{
+			mrt::Chunk video_data;
+			Finder->load(video_data, fname);
+			LOG_DEBUG(("video file loaded (%u bytes)", (unsigned)video_data.getSize()));
 
-		mpeg = SMPEG_new_data(video_data.getPtr(), video_data.getSize(), &mpeg_info, 0);
-		if (mpeg == NULL)
-			throw_sdl(("SMPEG_new_data: %s", SDL_GetError()));
+			mpeg = SMPEG_new_data(video_data.getPtr(), video_data.getSize(), &mpeg_info, 0);
+			if (mpeg == NULL)
+				throw_sdl(("SMPEG_new_data: %s", SDL_GetError()));
+		}
 
-		video_data.free();
-
-		shadow.createRGB(mpeg_info.width, mpeg_info.height, 24, SDL_SWSURFACE);
+		shadow.createRGB(screenshot->getWidth(), screenshot->getHeight(), 24, SDL_SWSURFACE);
 		shadow.fill(0);
-		frame.createRGB(mpeg_info.width, mpeg_info.height, 24, SDL_SWSURFACE);
+		frame.createRGB(screenshot->getWidth(), screenshot->getHeight(), 24, SDL_SWSURFACE);
 		frame.fill(0);
 		frame.setAlpha(0, 0);
 
@@ -57,6 +57,7 @@ base(base), name(name), mpeg(0), lock(SDL_CreateMutex()), active(false), updated
 		SMPEG_CHECK("SMPEG_enableaudio");
 		SMPEG_enablevideo(mpeg, 1);
 		SMPEG_CHECK("SMPEG_enablevideo");
+		
 		SDL_Surface *dst = shadow.getSDLSurface();
 		video_controls[dst] = this;
 		SMPEG_setdisplay(mpeg, dst, lock, update);
@@ -67,8 +68,8 @@ base(base), name(name), mpeg(0), lock(SDL_CreateMutex()), active(false), updated
 		
 		SMPEG_loop(mpeg, 1);
 		SMPEG_CHECK("SMPEG_loop");
-		SMPEG_play(mpeg);
-		SMPEG_CHECK("SMPEG_play");
+		//SMPEG_play(mpeg);
+		//SMPEG_CHECK("SMPEG_play");
 	}
 }
 
@@ -77,21 +78,25 @@ void VideoControl::tick(const float dt) {
 	if (mpeg == NULL) 
 		return;
 
-	int status = SMPEG_status(mpeg);
-	LOG_DEBUG(("status = %d", status));
-	
-	if (status == SMPEG_STOPPED) {
+	switch(SMPEG_status(mpeg)) {
+	case SMPEG_PLAYING: 
+		break;
+	case SMPEG_STOPPED: 
 		LOG_DEBUG(("calling SMPEG_play"));
 		SMPEG_play(mpeg);
+		break;
+	case SMPEG_ERROR: 
+		LOG_DEBUG(("SMPEG error: %s", SMPEG_error(mpeg)));
+		SMPEG_delete(mpeg);
+		mpeg = NULL;
+		return;
 	}
 	if (updated) {
-		LOG_DEBUG(("syncing frame with shadow"));
+		//LOG_DEBUG(("syncing frame with shadow"));
 		frame.createRGB(mpeg_info.width, mpeg_info.height, 24, SDL_SWSURFACE);
-		frame.fill(0);
 
 		SDL_mutexP(lock);
 		try {
-			int flags = shadow.getFlags();
 			shadow.setAlpha(0, 0);
 			frame.copyFrom(shadow, 0, 0);
 			frame.setAlpha(0, 0);
@@ -111,8 +116,9 @@ void VideoControl::render(sdlx::Surface &surface, const int x, const int y) {
 		surface.copyFrom(*screenshot, x, y);
 		return;
 	}
-	int dx = (screenshot->getWidth() - mpeg_info.width) / 2;
-	int dy = (screenshot->getHeight() - mpeg_info.height) / 2;
+	//int dx = (screenshot->getWidth() - mpeg_info.width) / 2;
+	//int dy = (screenshot->getHeight() - mpeg_info.height) / 2;
+	int dx = 0, dy = 0;
 	//LOG_DEBUG(("render %d %d", dx, dy));
 	
 	surface.copyFrom(frame, x + dx, y + dy);
