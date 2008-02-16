5abac595bbc2a4fd1a201ad162682f0bbf1c1dd3
Vladimir Menshakov <whoozle@yandex.ru>
2008-02-16 08:17:06 +0000

    more stubs
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@6329 5c0bf991-d514-0410-a65f-ce4832e571f3

--- engine/sl08/sl08.cpp
+++ engine/sl08/sl08.cpp
@@ -1,8 +1,22 @@
 #include "sl08.h"
+#include <stdio.h>
+
+sl08::signal1<void, int> signal;
+
+class Foo {
+	sl08::slot1<void, int, Foo> test_slot;
+public: 
+
+	Foo() : test_slot(this, &Foo::test) {
+		test_slot.connect(signal);
+	}
+	void test(int x) {
+		printf("test(%d)\n", x);
+	}
+};
 
 
-sl08::signal1<void, int> x;
-
-void test() {
-	x.emit(1);
+int main(const int argc, const char *argv[]) {
+	Foo foo;
+	signal.emit(1);
 }
--- engine/sl08/sl08.h
+++ engine/sl08/sl08.h
@@ -2,30 +2,71 @@
 #define SL08_SIGNALS_CORE_H
 
 #include <deque>
+#include <set>
 #include <algorithm>
 #include <functional>
 
-#define SIGNAL(N, tspec, proto, call) \
+#define SLOT(N, tspec, proto, call, signal_template, signal_spec) \
+template signal_template class signal##N; \
+template tspec \
+class slot##N { \
+	typedef signal##N signal_spec signal_type; \
+	signal_type *signal; \
+public: \
+	typedef R (CL::*func_t) proto; \
+	slot##N(CL *object, func_t func) : signal(NULL), object(object), func(func) {}\
+	\
+	R operator() proto { \
+		return (*object).*func call ;\
+	} \
+	\
+	void connect(signal##N signal_spec &signal_ref) {\
+		signal = &signal_ref; \
+		signal->connect(this); \
+	} \
+	\
+	void disconnect() {\
+		signal->disconnect(this); \
+	} \
+	~slot##N() { \
+		disconnect(); \
+	} \
+private: \
+	CL *object; \
+	func_t func;\
+}
+
+#define SIGNAL(N, tspec, proto, call, signal_spec, slot_spec) \
 template tspec \
 class signal##N { \
-	std::deque<void *> slots; \
-	struct caller : public std::unary_function<R, void *> { \
-		caller proto {} \
-		R operator()(void *a) {} \
-	    }; \
 public: \
-\
 	R emit proto { \
-		std::for_each(slots.begin(), slots.end(), caller call); \
+	} \
+\
+	template <class CL> \
+	void connect(slot##N slot_spec *slot) {\
+	} \
+	\
+	template <class CL> \
+	void disconnect(slot##N slot_spec *slot) {\
 	} \
 }
+#define SLOT_ARG0 <typename R, typename CL>
+#define SLOT_ARG1 <typename R, typename A1, typename CL>
 
 #define SIGNAL_ARG0 <typename R>
 #define SIGNAL_ARG1 <typename R, typename A1>
 
+#define SIGNAL_TEMPLATE_ARG0 <R>
+#define SIGNAL_TEMPLATE_ARG1 <R, A1>
+#define SLOT_TEMPLATE_ARG0 <R, CL>
+#define SLOT_TEMPLATE_ARG1 <R, A1, CL>
+
 namespace sl08 {
-	SIGNAL(0, SIGNAL_ARG0, (), ());
-	SIGNAL(1, SIGNAL_ARG1, (A1 a1), (a1));
+	SLOT(0, SLOT_ARG0, (), (), SIGNAL_ARG0, SIGNAL_TEMPLATE_ARG0);
+	SLOT(1, SLOT_ARG1, (A1 a1), (a1), SIGNAL_ARG1, SIGNAL_TEMPLATE_ARG1);
+	SIGNAL(0, SIGNAL_ARG0, (), (), TEMPLATE_ARG0, SLOT_TEMPLATE_ARG0);
+	SIGNAL(1, SIGNAL_ARG1, (A1 a1), (a1), TEMPLATE_ARG1, SLOT_TEMPLATE_ARG1);
 }
 
 #endif
