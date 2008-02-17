d6ed9294eb81e9ce7bf1e2fb4847f8e99e3fb188
Vladimir Menshakov <whoozle@yandex.ru>
2008-02-17 08:50:23 +0000

    finally get partially specialized version work
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@6337 5c0bf991-d514-0410-a65f-ce4832e571f3

--- engine/sl08/sl08.h
+++ engine/sl08/sl08.h
@@ -60,7 +60,7 @@ private: \
 	func_t func; \
 }
 
-#define SIGNAL(N, tspec, proto, call, signal_spec, slot_spec) \
+#define SIGNAL(N, tspec, proto, call, signal_spec, slot_spec, declist, arglist) \
 template tspec \
 class base_signal##N { \
 protected: \
@@ -88,37 +88,55 @@ public: \
 				i = slots.erase(i); \
 		} \
 	} \
-}; \
+};\
 template tspec \
 class signal##N : public base_signal##N signal_spec { \
 }
 
+#define SIGNAL_SPECIAL(N, typename_helper, proto, call, tspec, tpar) \
+template tspec \
+class signal##N tpar : public base_signal##N tpar { \
+	typedef base_signal##N tpar parent_t; \
+public: \
+	void emit proto {  \
+		for(typename_helper parent_t::slots_type::iterator i = parent_t::slots.begin(); i != parent_t::slots.end(); ++i) { \
+			(*i)->operator() call ; \
+		} \
+	} \
+}
+
+
 #define SLOT_ARG0 <typename R, class CL>
-#define SLOT_ARG1 <typename R, typename A1, class CL>
-
 #define SIGNAL_ARG0 <typename R>
-#define SIGNAL_ARG1 <typename R, typename A1>
-
 #define SIGNAL_TEMPLATE_ARG0 <R>
-#define SIGNAL_TEMPLATE_ARG1 <R, A1>
-
 #define SLOT_TEMPLATE_ARG0_ARGLIST ,
 #define SLOT_TEMPLATE_ARG0_ARGLIST2
 #define SLOT_TEMPLATE_ARG0 <R, ##SLOT_TEMPLATE_ARG0_ARGLIST## CL>
+#define SIGNAL_SPECIAL0_TEMPLATE <>
+#define SIGNAL_SPECIAL0_TPARAM <void>
 
-#define SLOT_TEMPLATE_ARG1_ARGLIST ,A1, 
-#define SLOT_TEMPLATE_ARG1_ARGLIST2 ,A1 
+#define SLOT_ARG1 <typename R, typename A1, class CL>
+#define SIGNAL_ARG1 <typename R, typename A1>
+#define SIGNAL_TEMPLATE_ARG1 <R, A1>
+#define SLOT_TEMPLATE_ARG1_ARGLIST , A1, 
+#define SLOT_TEMPLATE_ARG1_ARGLIST2 , A1 
 #define SLOT_TEMPLATE_ARG1_DECLIST typename A1,
 #define SLOT_TEMPLATE_ARG1_DECLIST2 typename A1
 #define SLOT_TEMPLATE_ARG1 <R, ##SLOT_TEMPLATE_ARG1_ARGLIST## CL>
+
+#define SIGNAL_SPECIAL1_TEMPLATE <typename A1>
+#define SIGNAL_SPECIAL1_TPARAM <void, A1>
 
 namespace sl08 {
 	SLOT(0, SLOT_ARG0, (), (), SIGNAL_ARG0, SIGNAL_TEMPLATE_ARG0, \
 		SLOT_TEMPLATE_ARG0_ARGLIST, SLOT_TEMPLATE_ARG0_ARGLIST2 ,,);
 	SLOT(1, SLOT_ARG1, (A1 a1), (a1), SIGNAL_ARG1, SIGNAL_TEMPLATE_ARG1, \
 		SLOT_TEMPLATE_ARG1_ARGLIST, SLOT_TEMPLATE_ARG1_ARGLIST2, SLOT_TEMPLATE_ARG1_DECLIST, SLOT_TEMPLATE_ARG1_DECLIST2);
-	SIGNAL(0, SIGNAL_ARG0, (), (), SIGNAL_TEMPLATE_ARG0, SLOT_TEMPLATE_ARG0);
-	SIGNAL(1, SIGNAL_ARG1, (A1 a1), (a1), SIGNAL_TEMPLATE_ARG1, SLOT_TEMPLATE_ARG1);
+	SIGNAL(0, SIGNAL_ARG0, (), (), SIGNAL_TEMPLATE_ARG0, SLOT_TEMPLATE_ARG0, SIGNAL_TEMPLATE_ARG0_DECLIST, SLOT_TEMPLATE_ARG0_ARGLIST2);
+	SIGNAL(1, SIGNAL_ARG1, (A1 a1), (a1), SIGNAL_TEMPLATE_ARG1, SLOT_TEMPLATE_ARG1, SIGNAL_TEMPLATE_ARG1_DECLIST, SLOT_TEMPLATE_ARG1_ARGLIST2);
+
+	SIGNAL_SPECIAL(0, , 		 (), (), SIGNAL_SPECIAL0_TEMPLATE, SIGNAL_SPECIAL0_TPARAM);
+	SIGNAL_SPECIAL(1, typename , (A1 a1), (a1), SIGNAL_SPECIAL1_TEMPLATE, SIGNAL_SPECIAL1_TPARAM);
 }
 
 #endif
