b0102443724b9ec522e061f70be0030aec8aedeb
Vladimir Menshakov <whoozle@yandex.ru>
2008-02-22 10:19:41 +0000

    minor cleanup
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@6377 5c0bf991-d514-0410-a65f-ce4832e571f3

--- sdlx/glSDL/d3dsdl.cpp
+++ sdlx/glSDL/d3dsdl.cpp
@@ -143,7 +143,7 @@ SDL_Surface *d3dSDL_SetVideoMode(int width, int height, int bpp, Uint32 flags) {
 		LOG_DEBUG(("format = %d", (int) d3ddm.Format));
         d3dpp.Windowed         = TRUE;
         d3dpp.BackBufferFormat = d3ddm.Format;
-	    //d3dpp.PresentationInterval   = D3DPRESENT_INTERVAL_IMMEDIATE;
+	   // d3dpp.PresentationInterval   = D3DPRESENT_INTERVAL_IMMEDIATE;
     }
     else
     {
@@ -155,10 +155,10 @@ SDL_Surface *d3dSDL_SetVideoMode(int width, int height, int bpp, Uint32 flags) {
 	    d3dpp.PresentationInterval   = D3DPRESENT_INTERVAL_IMMEDIATE; // Do NOT sync to vertical retrace
     }
 	
+	//d3dpp.BackBufferCount = 2;
 	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD; //we do not use any back buffers - redrawing full scene from scratch
-	//d3dpp.BackBufferCount = 1;
     d3dpp.EnableAutoDepthStencil = FALSE;
-    d3dpp.AutoDepthStencilFormat = D3DFMT_D24X8; //D3DFMT_D16;
+    d3dpp.AutoDepthStencilFormat = D3DFMT_D16; //D3DFMT_D24X8; 
     d3dpp.Flags                  = 0; //D3DPRESENTFLAG_DISCARD_DEPTHSTENCIL; //D3DPRESENTFLAG_LOCKABLE_BACKBUFFER;
 
     HRESULT r;
