8a8403191da2fa299b8d9a0c036cfd70cbd5fa0f
Vladimir Menshakov <whoozle@yandex.ru>
2008-03-06 10:05:46 +0000

    added close()
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@6453 5c0bf991-d514-0410-a65f-ce4832e571f3

--- sdlx/file_rw.cpp
+++ sdlx/file_rw.cpp
@@ -7,18 +7,31 @@
 static int mrt_seek(struct SDL_RWops *context, int offset, int whence) {
 	assert(context->hidden.unknown.data1 != NULL);
 	const mrt::BaseFile *file = (const mrt::BaseFile *)context->hidden.unknown.data1;
-	file->seek(offset, whence);
-	return file->tell();
+	TRY {
+		file->seek(offset, whence);
+		return file->tell();
+	} CATCH("mrt_seek", return -1);
 }
 
 static int mrt_read(struct SDL_RWops *context, void *ptr, int size, int maxnum) {
 	assert(context->hidden.unknown.data1 != NULL);
 	const mrt::BaseFile *file = (const mrt::BaseFile *)context->hidden.unknown.data1;
-	int r = file->read(ptr, size * maxnum);
-	return (r > 0) ? r / size : r;
+	TRY {
+		int r = file->read(ptr, size * maxnum);
+		return (r > 0) ? r / size : r;
+	} CATCH("mrt_read", return -1);
 }
 
-SDL_RWops * sdlx::RWFromMRTFile(const mrt::BaseFile *file) {
+static int mrt_close(struct SDL_RWops *context) {
+	assert(context->hidden.unknown.data1 != NULL);
+	mrt::BaseFile *file = (mrt::BaseFile *)context->hidden.unknown.data1;
+	TRY {
+		file->close();
+		return 0;
+	} CATCH("mrt_close", return -1;);
+}
+
+SDL_RWops * sdlx::RWFromMRTFile(mrt::BaseFile *file) {
 	SDL_RWops * op = SDL_AllocRW();
 	if (op == NULL)
 		throw_sdl(("SDL_AllocRW()"));
@@ -28,6 +41,6 @@ SDL_RWops * sdlx::RWFromMRTFile(const mrt::BaseFile *file) {
 	op->seek = &mrt_seek;
 	op->read = &mrt_read;
 	op->write = NULL;
-	op->close = NULL;
+	op->close = &mrt_close;
 	return op;
 }
--- sdlx/file_rw.h
+++ sdlx/file_rw.h
@@ -9,7 +9,7 @@ namespace mrt {
 
 namespace sdlx {
 	
-	SDL_RWops * SDLXAPI RWFromMRTFile(const mrt::BaseFile *); //no close! 
+	SDL_RWops * SDLXAPI RWFromMRTFile(mrt::BaseFile *); //no close! 
 
 }
 
