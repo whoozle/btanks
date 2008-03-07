648895246240cfd6127cb627381fca2a93d2b2ef
Vladimir Menshakov <whoozle@yandex.ru>
2008-03-07 10:13:08 +0000

    added locker everywhere
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@6477 5c0bf991-d514-0410-a65f-ce4832e571f3

--- clunk/object.cpp
+++ clunk/object.cpp
@@ -1,27 +1,33 @@
 #include "object.h"
 #include "context.h"
+#include "locker.h"
 
 using namespace clunk;
 
 Object::Object(Context *context) : context(context) {}
 
 void Object::update(const v3<float> &pos, const v3<float> &vel) {
+	AudioLocker l;
 	position = pos;
 	velocity = vel;
 }
 
 void Object::play(Source *source) {
+	AudioLocker l;
 	sources.insert(source);
 }
 
 void Object::remove(Source *source) {
+	AudioLocker l;
 	sources.erase(source);
 }
 
 void Object::remove_all() {
+	AudioLocker l;
 	sources.clear();
 }
 
 Object::~Object() {
+	AudioLocker l;
 	context->delete_object(this);
 }
