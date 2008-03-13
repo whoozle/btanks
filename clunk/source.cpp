59a8a8ab506e68d7fb0e0c6e7c0be41fc6fcd4bd
Vladimir Menshakov <whoozle@yandex.ru>
2008-03-13 13:49:28 +0000

    removed noisy debug
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@6507 5c0bf991-d514-0410-a65f-ce4832e571f3

--- clunk/source.cpp
+++ clunk/source.cpp
@@ -84,7 +84,7 @@ void Source::hrtf(mrt::Chunk &result, int dst_n, const Sint16 *src, int src_ch, 
 		}
 		kiss_fftri(kiss_cfg_i, freq, src_data);
 		for(int j = 0; j < WINDOW_SIZE; ++j) {
-			LOG_DEBUG(("%g", src_data[j]));
+			//LOG_DEBUG(("%g", src_data[j]));
 			//tr[pos + j] /= 512; 
 			dst[i * WINDOW_SIZE + j] = (Sint16)(src_data[j] / WINDOW_SIZE * 32767);
 			//printf("%g,%g ", tr[pos + j], tr[pos + j] / 1024);
