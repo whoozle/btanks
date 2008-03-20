f6b9e7305c0ab90226017922baf6f79ec85aea3c
Vladimir Menshakov <whoozle@yandex.ru>
2008-03-20 12:39:27 +0000

    added stream stub
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@6533 5c0bf991-d514-0410-a65f-ce4832e571f3

---
+++ clunk/stream.h
@@ -0,0 +1,19 @@
+#ifndef CLUNK_STREAM_H__
+#define CLUNK_STREAM_H__
+
+#include "export_clunk.h"
+
+namespace mrt {
+	class Chunk;
+};
+
+namespace clunk {
+class CLUNKAPI Stream {
+public: 
+	virtual bool read(mrt::Chunk &data) = 0;
+	virtual ~Stream();
+};
+}
+
+#endif
+
