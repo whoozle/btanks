f57f295c4f8871f0bb62fd6881f3b0f4af0fe864
Vladimir Menshakov <whoozle@yandex.ru>
2008-03-26 07:37:03 +0000

    cleaned up targets, added pillbox to target list
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@6648 5c0bf991-d514-0410-a65f-ce4832e571f3

--- engine/ai/targets.cpp
+++ engine/ai/targets.cpp
@@ -11,26 +11,29 @@ void ITargets::insert(std::set<std::string> &x, const char * array[]) {
 }
 
 ITargets::ITargets() {
-	const char *a1[] = {"fighting-vehicle", NULL};
-	insert(players, a1);
-	const char *a1_1[] = {"fighting-vehicle", "monster", NULL};
-	insert(players_and_monsters, a1_1);
+	players.insert("fighting-vehicle");
 
-	const char *a2[] = {"cannon", "fighting-vehicle", "trooper", "kamikaze", "boat", "helicopter", "monster", "watchtower", NULL};
+	players_and_monsters = players;
+	players_and_monsters.insert("monster");
+
+	const char *a2[] = {"cannon", "pillbox", "fighting-vehicle", "trooper", "kamikaze", "boat", "helicopter", "monster", "watchtower", NULL};
 	insert(troops, a2);
-	const char *a2_1[] = {"cannon", "fighting-vehicle", "trooper", "kamikaze", "boat", "helicopter", "monster", "watchtower", "train", NULL};
-	insert(troops_and_train, a2_1);
 
-	const char *a3[] = {"cannon", "fighting-vehicle", "trooper", "kamikaze", "boat", "helicopter", "monster", "watchtower", "missile", NULL};
-	insert(troops_and_missiles, a3);
-	const char *a3_1[] = {"cannon", "fighting-vehicle", "trooper", "kamikaze", "boat", "helicopter", "monster", "watchtower", "missile", "train", NULL};
-	insert(troops_train_and_missiles, a3_1);
+	troops_and_train = troops;
+	troops_and_train.insert("train");
 
+	troops_and_missiles = troops;
+	troops_and_missiles.insert("missile");
+
+	troops_train_and_missiles = troops_and_train;
+	troops_train_and_missiles.insert("missile");
+	
 	const char *a4[] = {"fighting-vehicle", "trooper", "cannon", "watchtower", "creature", "civilian", NULL};
 	insert(monster, a4);
 
 	const char *a5[] = {"fighting-vehicle", "trooper", "monster", "kamikaze", NULL};
 	insert(infantry, a5);
-	const char *a6[] = {"fighting-vehicle", "trooper", "monster", "kamikaze", "train", NULL};
-	insert(infantry_and_train, a6);
+
+	infantry_and_train = infantry;
+	infantry_and_train.insert("train");
 }
