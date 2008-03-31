2f77433c57d1bd94c2e0237549bbd4a421c55316
Vladimir Menshakov <whoozle@yandex.ru>
2008-03-31 10:50:12 +0000

    fixed variant name
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@6691 5c0bf991-d514-0410-a65f-ce4832e571f3

--- objects/shilka.cpp
+++ objects/shilka.cpp
@@ -175,7 +175,7 @@ skip_left_toggle:
 			int n;
 			Config->get("objects.shilka.units-limit", n, 10); //fixme: add type restrictions
 			if (mod->getCount() > 0 && getChildren("trooper") < n) {
-				spawn(mod_type + "(disembark)" + (RTConfig->game_type == GameTypeCooperative? "ally":""), mod_type, _direction*(size.length()/-2), v2<float>());
+				spawn(mod_type + "(disembark)" + (RTConfig->game_type == GameTypeCooperative? "(ally)":""), mod_type, _direction*(size.length()/-2), v2<float>());
 				mod->decreaseCount();
 			}
 		}
