3cfe61392c7d97dd4d53d462fd3dbb90e656ac74
Vladimir Menshakov <whoozle@yandex.ru>
2008-04-04 11:47:55 +0000

    added diagonal delaying magic
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@6780 5c0bf991-d514-0410-a65f-ce4832e571f3

--- engine/controls/control_method.cpp
+++ engine/controls/control_method.cpp
@@ -1,14 +1,25 @@
 #include "control_method.h"
 
 ControlMethod::ControlMethod() : _release_set(false), _release_timer(false) {
-	_release_timer.set(0.01f);
+	_release_timer.set(0.1f);
 }
 
 void ControlMethod::updateState(PlayerSlot &slot, PlayerState &state, const float dt) {
 	_updateState(slot, state);
-	return;
 	
-	if (state.compare_directions(_old_state)) {
+	int dirs = 0;
+	dirs += state.left;
+	dirs += state.right;
+	dirs += state.up;
+	dirs += state.down;
+
+	int old_dirs = 0;
+	old_dirs += _old_state.left;
+	old_dirs += _old_state.right;
+	old_dirs += _old_state.up;
+	old_dirs += _old_state.down;
+	
+	if (state.compare_directions(_old_state) || (old_dirs != 2 || dirs != 1)) {
 		_old_state = state;
 		return;
 	}
@@ -17,12 +28,21 @@ void ControlMethod::updateState(PlayerSlot &slot, PlayerState &state, const floa
 		LOG_DEBUG(("setting release timer"));
 		_release_timer.reset();
 		_release_set = true;
+		state = _old_state;
 		return;
 	} else if (_release_timer.tick(dt)) {
 		LOG_DEBUG(("release timer stopped"));
 		_old_state = state;
 		_release_set = false;	
 	} else {
+		if ((!state.left && _old_state.left) || (!state.right && _old_state.right)) {
+			if ((!state.up && _old_state.up) || (!state.down && _old_state.down)) {
+				LOG_DEBUG(("atomically update diagonal"));
+				_old_state = state;
+				_release_set = false;
+				return;
+			}
+		}
 		state = _old_state;
 		return;
 	}
