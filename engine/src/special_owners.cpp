5b494b7ce7df8f668571df88cdfc4f98b65dd6f2
Vladimir Menshakov <whoozle@yandex.ru>
2008-04-18 13:30:28 +0000

    added get_team/get_team_color
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@6894 5c0bf991-d514-0410-a65f-ce4832e571f3

---
+++ engine/src/special_owners.cpp
@@ -0,0 +1,34 @@
+#include "special_owners.h"
+#include "object.h"
+
+const char * get_team_color(TeamID t) {
+	switch(t) {
+		case TeamRed:
+			return "red";
+		case TeamGreen: 
+			return "green";
+		case TeamBlue: 
+			return "blue";
+		case TeamYellow: 
+			return "yellow";
+		default: 
+			return "unknown";
+	}
+}
+
+TeamID get_team(const Object *o) {
+	if (o->animation.compare(0, 5, "flag-") != 0 && o->animation.compare(0, 8, "ctf-base") != 0)
+		return TeamNone;
+
+	size_t l = o->animation.size();
+	if (o->animation.compare(l - 4, 4, "-red") == 0) {
+		return TeamRed;
+	} else if (o->animation.compare(l - 6, 6, "-green") == 0) {
+		return TeamGreen;
+	} else if (o->animation.compare(l - 5, 5, "-blue") == 0) {
+		return TeamBlue;
+	} else if (o->animation.compare(l - 7, 7, "-yellow") == 0) {
+		return TeamYellow;
+	} 
+	return TeamNone;
+}
