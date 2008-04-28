c4e240ad86073fc578f17c6f81085a7ea3748afa
Vladimir Menshakov <whoozle@yandex.ru>
2008-04-28 12:05:39 +0000

    check for prefix and suffix
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@6992 5c0bf991-d514-0410-a65f-ce4832e571f3

--- engine/src/team.cpp
+++ engine/src/team.cpp
@@ -17,17 +17,14 @@ const char * Team::get_color(ID t) {
 }
 
 Team::ID Team::get_team(const Object *o) {
-	if (o->animation.compare(0, 5, "flag-") != 0 && o->animation.compare(0, 8, "ctf-base") != 0)
-		return None;
-
 	size_t l = o->animation.size();
-	if (o->animation.compare(l - 4, 4, "-red") == 0) {
+	if (o->animation.compare(l - 4, 4, "-red") == 0 || o->animation.compare(0, 4, "red-") == 0) {
 		return Red;
-	} else if (o->animation.compare(l - 6, 6, "-green") == 0) {
+	} else if (o->animation.compare(l - 6, 6, "-green") == 0 || o->animation.compare(0, 6, "green-") == 0) {
 		return Green;
-	} else if (o->animation.compare(l - 5, 5, "-blue") == 0) {
+	} else if (o->animation.compare(l - 5, 5, "-blue") == 0 || o->animation.compare(0, 5, "blue-") == 0) {
 		return Blue;
-	} else if (o->animation.compare(l - 7, 7, "-yellow") == 0) {
+	} else if (o->animation.compare(l - 7, 7, "-yellow") == 0 || o->animation.compare(0, 7, "yellow-") == 0) {
 		return Yellow;
 	} 
 	return None;
