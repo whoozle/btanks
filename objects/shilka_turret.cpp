edfec60863ce51afa8e2947a650104bbd680dc22
Vladimir Menshakov <whoozle@yandex.ru>
2008-05-21 11:53:30 +0000

    added turrent stub
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@7256 5c0bf991-d514-0410-a65f-ce4832e571f3

---
+++ objects/shilka_turret.cpp
@@ -0,0 +1,48 @@
+/* Battle Tanks Game
+ * Copyright (C) 2006-2008 Battle Tanks team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "object.h"
+#include "registrar.h"
+
+class ShilkaTurret : public Object {
+public:
+	void tick(const float dt) {
+		Object::tick(dt);
+	}
+	
+	CTFBase() : Object("turrel") {
+		impassability = 0;
+		hp = -1;
+		setDirectionsNumber(16);
+		pierceable = true;
+	}
+
+	void calculate(const float dt) {
+		
+	}
+	
+	virtual Object * clone() const { return new ShilkaTurret(*this); }
+	
+	void onSpawn() {
+		play("main", true);
+	}
+
+private:
+};
+
+REGISTER_OBJECT("shilka-turret", ShilkaTurret, ());
