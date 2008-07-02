10d69609dd0e2d5ac200b54f5d0bd7da3f78d631
Vladimir Menshakov <whoozle@yandex.ru>
2008-07-02 05:56:51 +0000

    remove bogus code
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@7427 5c0bf991-d514-0410-a65f-ce4832e571f3

--- editor/layer_list_dialog.cpp
+++ editor/layer_list_dialog.cpp
@@ -17,7 +17,6 @@ LayerListDialog::LayerListDialog(const int w, const int h) : ScrollList("menu/ba
 }
 
 void LayerListDialog::initMap() {
-	int idx = get();
 	clear();
 	std::set<int> layers; 
 	Map->getLayers(layers);
@@ -26,11 +25,6 @@ void LayerListDialog::initMap() {
 		Layer * layer = Map->getLayer(z);
 		append(new LayerItem(z, layer));
 	}
-	if (idx >= size()) {
-		idx = size() - 1;
-	}
-	if (!empty())
-		set(idx); 
 }
 
 const LayerItem * LayerListDialog::getItem(const int idx) const {
