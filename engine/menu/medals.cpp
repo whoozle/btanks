b42e821b7221c87d4bb91f412c4ac460936927e7
Vladimir Menshakov <whoozle@yandex.ru>
2008-09-22 13:39:22 +0000

    added medal stub
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@7776 5c0bf991-d514-0410-a65f-ce4832e571f3

---
+++ engine/menu/medals.cpp
@@ -0,0 +1,2 @@
+#include "medals.h"
+
---
+++ engine/menu/medals.h
@@ -0,0 +1,11 @@
+#ifndef BTANKS_MENU_MEDALS_H__
+#define BTANKS_MENU_MEDALS_H__
+
+#include "container.h"
+
+class Medals : public Container {
+public: 
+};
+
+#endif
+
