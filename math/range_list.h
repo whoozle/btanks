e7f434dbe9efcef7e3587ae02a9cb1498dad4272
Vladimir Menshakov <whoozle@yandex.ru>
2008-10-24 14:15:55 +0000

    fixed range packing in a linear way
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@7909 5c0bf991-d514-0410-a65f-ce4832e571f3

--- math/range_list.h
+++ math/range_list.h
@@ -8,6 +8,45 @@ class range_list : public std::map<const T, T> {
 public: 
 	typedef std::map<const T, T> parent_type;
 
+private: 
+	typename parent_type::iterator pack_left(typename parent_type::iterator i) {
+		if (i == parent_type::begin())
+			return i;
+
+		typename parent_type::iterator prev = i;
+		--prev;
+
+		if (prev->second + 1 < i->first)
+			return i;
+		
+		T e = i->second;
+		
+		parent_type::erase(i);
+		prev->second = e;
+
+		return pack_left(prev);
+	}
+
+	typename parent_type::iterator pack_right(typename parent_type::iterator i) {
+		if (i == parent_type::end())
+			return i;
+
+		typename parent_type::iterator next = i;
+		++next;
+		if (next == parent_type::end())
+			return i;
+		
+		if (i->second + 1 < next->first)
+			return i;
+
+		T e = next->second;
+		parent_type::erase(next);
+		i->second = e;
+
+		return pack_right(i);
+	}
+
+public: 
 	void insert(const T& value) {
 		if (parent_type::empty()) {
 			parent_type::insert(typename parent_type::value_type(value, value));
@@ -22,8 +61,8 @@ public:
 			if (value + 1 == i->first) {
 				T e = i->second;
 				erase(i);
-				parent_type::insert(typename parent_type::value_type(value, e)); //expand beginning
-				return;
+				i = parent_type::insert(typename parent_type::value_type(value, e)).first; //expand beginning
+				i = pack_left(i);
 			}
 		}
 		
@@ -35,6 +74,7 @@ public:
 		
 		if (i->second + 1 == value) {
 			i->second = value;
+			i = pack_right(i);
 			return;
 		}
 		
