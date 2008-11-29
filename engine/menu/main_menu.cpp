7a13c9ae5a1886f874d06acc3bd6f8218f191853
Vladimir Menshakov <whoozle@yandex.ru>
2008-11-29 13:16:36 +0000

    added main menu
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@7956 5c0bf991-d514-0410-a65f-ce4832e571f3

---
+++ engine/menu/main_menu.cpp
@@ -0,0 +1,109 @@
+#include "main_menu.h"
+#include "campaign_menu.h"
+#include "start_server_menu.h"
+#include "join_server_menu.h"
+#include "options_menu.h"
+#include "menuitem.h"
+
+#include "resource_manager.h"
+#include "sdlx/font.h"
+#include "sound/mixer.h"
+
+bool MainMenu::generate_key_events_for_gamepad;
+
+MainMenu::MainMenu(int w, int h) : active(NULL) {
+	
+	CampaignMenu * cm = new CampaignMenu(w, h);
+	if (cm->empty()) {
+		delete cm;
+		cm = NULL;
+	} else {
+		add(new MenuItem("big", "menu", "start-campaign"), cm);
+	}
+	
+	add(new MenuItem("big", "menu", "start-game"), new StartServerMenu(w, h));
+	add(new MenuItem("big", "menu", "join-game"), new JoinServerMenu(w, h));
+	add(new MenuItem("big", "menu", "options"), new OptionsMenu(w, h));
+	
+	add(new MenuItem("big", "menu", "credits"));
+	add(new MenuItem("big", "menu", "quit"));
+
+	int mw, mh;
+	get_size(mw, mh);
+	
+	const sdlx::Font * font = ResourceManager->loadFont("big", true);
+
+	background.init("menu/background_box.png", w + 128, mh + 32, font->get_height() + 2);
+	dx = (w - mw) / 2;
+	dy = (h - mh) / 2;
+}
+
+void MainMenu::add(MenuItem *item, Control *slave) {
+	Menu::add(item);
+	items.push_back(slave);
+}
+
+void MainMenu::tick(const float dt) {
+	if (changed()) {
+		reset();
+		LOG_DEBUG(("changed %d", current_item));
+		if (current_item >=0 && current_item < (int)items.size() && (active = items[current_item]) != NULL) {
+			hide();
+			active->hide(false);
+		} else {
+			active = NULL;
+			MenuItem * item = dynamic_cast<MenuItem *>(get_current_item());
+			if (item != NULL)
+				menu_signal.emit(item->get_id());
+		}
+	}
+	if (active != NULL) {
+		if (active->hidden()) {
+			active = NULL;
+			Mixer->playSample(NULL, "menu/return.ogg", false);
+		} else 
+			active->tick(dt);
+	}
+}
+
+void MainMenu::render(sdlx::Surface &surface, const int x, const int y) const {
+	if (active != NULL && !active->hidden())
+		active->render(surface, x, y);
+	else
+		Menu::render(surface, x + dx, y + dy);
+}
+
+bool MainMenu::onKey(const SDL_keysym sym) {
+	if (active != NULL && !active->hidden()) 
+		return active->onKey(sym);
+	else 
+		return Menu::onKey(sym);
+}
+
+bool MainMenu::onMouse(const int button, const bool pressed, const int x, const int y) {
+	if (active != NULL && !active->hidden())
+		return active->onMouse(button, pressed, x, y);
+	else 
+		return Menu::onMouse(button, pressed, x, y);
+}
+
+bool MainMenu::onMouseMotion(const int state, const int x, const int y, const int xrel, const int yrel) {
+	if (active != NULL && !active->hidden())
+		return active->onMouseMotion(state, x, y, xrel, yrel);
+	else
+		return Menu::onMouseMotion(state, x, y, xrel, yrel);
+}
+
+void MainMenu::on_mouse_enter(bool enter) {
+	if (active != NULL && !active->hidden())
+		active->on_mouse_enter(enter);
+	else
+		Container::on_mouse_enter(enter);
+}
+
+MainMenu::~MainMenu() {
+	for(size_t i = 0; i < items.size(); ++i) {
+		delete items[i];
+	}
+}
+
---
+++ engine/menu/main_menu.h
@@ -0,0 +1,60 @@
+#ifndef BTANKS_MENU_MAIN_MENU_DESC_H__
+#define BTANKS_MENU_MAIN_MENU_DESC_H__
+
+/* Battle Tanks Game
+ * Copyright (C) 2006-2008 Battle Tanks team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/* 
+ * Additional rights can be granted beyond the GNU General Public License 
+ * on the terms provided in the Exception. If you modify this file, 
+ * you may extend this exception to your version of the file, 
+ * but you are not obligated to do so. If you do not wish to provide this
+ * exception without modification, you must delete this exception statement
+ * from your version and license this file solely under the GPL without exception. 
+*/
+
+#include "menu.h"
+#include "sl08/sl08.h"
+
+class MainMenu : public Menu {
+public: 
+	sl08::signal1<void, const std::string &, MainMenu> menu_signal; //backward compatibility, remove later
+
+
+	MainMenu(int w, int h);
+	static bool generate_key_events_for_gamepad;
+	virtual void tick(const float dt);
+
+	void add(MenuItem *item, Control *slave = NULL);
+
+	virtual bool onKey(const SDL_keysym sym);
+	virtual bool onMouse(const int button, const bool pressed, const int x, const int y);
+	virtual bool onMouseMotion(const int state, const int x, const int y, const int xrel, const int yrel);
+	virtual void on_mouse_enter(bool enter = true);
+
+	virtual void render(sdlx::Surface &surface, const int x, const int y) const;
+	~MainMenu();
+
+private: 
+	std::vector<Control *> items;
+	Control * active;
+	
+	int dx, dy;
+};
+
+#endif
