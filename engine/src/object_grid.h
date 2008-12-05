18cab4a8f8ca4c3e7c21655a8a443f3a50261c85
Vladimir Menshakov <whoozle@yandex.ru>
2008-12-05 00:11:57 +0000

    restored old grid
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@7995 5c0bf991-d514-0410-a65f-ce4832e571f3

--- engine/src/object_grid.h
+++ engine/src/object_grid.h
@@ -34,76 +34,77 @@
 #include <algorithm>
 #include "math/v2.h"
 #include "math/binary.h"
-#include "math/quad_tree.h"
+#include "mrt/hash_compat.h"
 
-template<typename T, int C = 32> 
+template<typename T> 
 class Grid {
 public: 
 	Grid() {}
 
 	void set_size(const v2<int> &size, const int step, const bool wrap) {
 		clear();
+		_grid_size = v2<int>(step, step);
+		resize(_grid, _grid_size, size);
+		_grid4_size = v2<int>(step * 4, step * 4);
+		resize(_grid4, _grid4_size, size);
 		_wrap = wrap;
-		tree.set_size(size.x, size.y);
+		_map_size = size;
 	}
 
 	void clear() {
-		tree.clear();
+		_grid.clear();
+		_grid4.clear();
 		_index.clear();
-		_wrap = false;
 	}
 
 	void update(T id, const v2<int> &pos, const v2<int> &size) {
+		if (_grid.empty())
+			return;
+		
 		typename Index::iterator i = _index.find(id);
 		if (i != _index.end()) {
-			v2<int> pos2 = pos + size;
+		//skip modification if grid coordinates
+			if (pos / _grid_size == i->second.pos / _grid_size &&
+				(pos + size - 1) / _grid_size == (i->second.pos + size - 1) / _grid_size) {
+				return;	
+			}
 			
-			Object &o = i->second;
-			if (o.pos == pos && o.pos2 == pos2) 
-				return;
 
-			tree.erase(value_type(o.pos.x, o.pos.y, id)); //ul
-			tree.insert(value_type(pos.x, pos.y, id));
-			
-			tree.erase(value_type(o.pos2.x, o.pos2.y, id)); //dr
-			tree.insert(value_type(pos2.x, pos2.y, id));
+			removeFromGrid(_grid, _grid_size, id, i->second);
+			removeFromGrid(_grid4, _grid4_size, id, i->second);
+			i->second.pos = pos;
+			i->second.size = size;
+		} else 
+			_index.insert(typename Index::value_type(id, Object(pos, size)));
 
-			tree.erase(value_type(o.pos.x, o.pos2.y, id)); //dl
-			tree.insert(value_type(pos.x, pos2.y, id));
-			
-			tree.erase(value_type(o.pos2.x, o.pos.y, id)); //ur
-			tree.insert(value_type(pos2.x, pos.y, id));
-
-			o.pos = pos;
-			o.pos2 = pos2;
-		} else {
-			Object o(pos, size);
-			_index.insert(typename Index::value_type(id, o));
-			
-			tree.insert(value_type(o.pos.x, o.pos.y, id));
-			tree.insert(value_type(o.pos2.x, o.pos2.y, id));
-
-			tree.insert(value_type(o.pos.x, o.pos2.y, id));
-			tree.insert(value_type(o.pos2.x, o.pos.y, id));
-		}
+		update(_grid, _grid_size, id, pos, size);
+		update(_grid4, _grid4_size, id, pos, size);
 	}
 
 	void remove(T id) {
+		if (_grid.empty())
+			return;
+
 		typename Index::iterator i = _index.find(id);
 		if (i != _index.end()) {
-			Object &o = i->second;
-			tree.erase(value_type(o.pos.x, o.pos.y, id));
-			tree.erase(value_type(o.pos2.x, o.pos2.y, id));
-
-			tree.erase(value_type(o.pos.x, o.pos2.y, id));
-			tree.erase(value_type(o.pos2.x, o.pos.y, id));
-			
+			removeFromGrid(_grid, _grid_size, id, i->second);		
+			removeFromGrid(_grid4, _grid4_size, id, i->second);		
 			_index.erase(i);
-		}
+		} 
 	}
 
 	void collide(std::set<T> &objects, const v2<int>& area_pos, const v2<int>& area_size) const {
-		tree.search(objects, area_pos.x, area_pos.y, area_pos.x + area_size.x, area_pos.y + area_size.y);
+		if (_grid.empty())
+			return;
+
+		v2<int> size = (area_size - 1) / _grid_size + 1;
+		int n = size.x * size.y;
+		if (n >= 16) { //replace with config ? 
+			collide(objects, _grid4, _grid4_size, area_pos, area_size);	
+		} else {
+			collide(objects, _grid, _grid_size, area_pos, area_size);		
+		}
+		//LOG_DEBUG(("returned %u objects", (unsigned)objects.size()));
 	}
 	
 private:
@@ -112,11 +113,98 @@ private:
 		return x < 0? x + y: x;
 	}
 
+	typedef std::set<T> IDSet;
+	typedef std::vector<IDSet> SetVector;
+	typedef std::vector<SetVector> GridMatrix;
+
 	struct Object {
-		Object(const v2<int>& pos, const v2<int>& size) : pos(pos), pos2(pos + size) {}
-		v2<int> pos, pos2;
+		Object(const v2<int>& pos, const v2<int>& size) : pos(pos), size(size) {}
+		v2<int> pos, size;
 	};
 
+	void collide(std::set<T> &result, const GridMatrix &grid, const v2<int> &grid_size, const v2<int>& area_pos, const v2<int>& area_size) const {
+		v2<int> delta = v2<int>(grid[0].size(), grid.size()) * grid_size - _map_size;
+		
+		const v2<int> start = area_pos / grid_size;
+		v2<int> end = (area_pos + area_size - 1) / grid_size;
+
+		if (end.y < (int)grid.size() - 1) delta.y = 0;
+		if (end.x < (int)grid[0].size() - 1) delta.x = 0;
+		end = (area_pos + area_size + delta - 1) / grid_size;
+
+		const int y1 = _wrap? start.y: math::max(0, start.y);
+		const int y2 = _wrap? end.y: math::min((int)grid.size() - 1, end.y);
+		const int x1 = _wrap? start.x: math::max(0, start.x);
+		for(int y = y1; y <= y2; ++y) {
+			const SetVector &row = grid[wrap(y, grid.size())];
+			const int x2 = _wrap?end.x: math::min((int)row.size() - 1, end.x);
+			for(int x = x1; x <= x2; ++x) {
+				const IDSet &set = row[wrap(x, row.size())];
+				result.insert(set.begin(), set.end());
+			}
+		}
+	}
+
+	void removeFromGrid(GridMatrix &grid, const v2<int> &grid_size, T id, const Object &o) {
+		v2<int> delta = v2<int>(grid[0].size(), grid.size()) * grid_size - _map_size;
+
+		const v2<int> start = o.pos / grid_size;
+		v2<int> end = (o.pos + o.size - 1) / grid_size;
+
+		if (end.y < (int)grid.size() - 1) delta.y = 0;
+		if (end.x < (int)grid[0].size() - 1) delta.x = 0;
+		end = (o.pos + o.size + delta - 1) / grid_size;
+
+		const int y1 = _wrap? start.y: math::max(0, start.y);
+		const int y2 = _wrap? end.y: math::min((int)grid.size() - 1, end.y);
+		const int x1 = _wrap? start.x: math::max(0, start.x);
+		for(int y = y1; y <= y2; ++y) {
+			SetVector &row = grid[wrap(y, grid.size())];
+			const int x2 = _wrap? end.x: math::min((int)row.size() - 1, end.x);
+			for(int x = x1; x <= x2; ++x) {
+				row[wrap(x, row.size())].erase(id);
+			}
+		}
+	}
+
+	void update(GridMatrix &grid, const v2<int> &grid_size, T id, const v2<int> &pos, const v2<int> &size) {
+		v2<int> delta = v2<int>(grid[0].size(), grid.size()) * grid_size - _map_size;
+		
+		const v2<int> start = pos / grid_size;
+		v2<int> end = (pos + size - 1) / grid_size;
+		
+		if (end.y < (int)grid.size() - 1) delta.y = 0;
+		if (end.x < (int)grid[0].size() - 1) delta.x = 0;
+		end = (pos + size + delta - 1) / grid_size;
+		
+		//LOG_DEBUG(("updating %d (%d, %d) -> (%d, %d) (%d %d)", id, start.x, start.y, end.x, end.y, pos.x, pos.y));
+		
+		const int y1 = _wrap? start.y: math::max(0, start.y);
+		const int y2 = _wrap? end.y: math::min((int)grid.size() - 1, end.y);
+		const int x1 = _wrap? start.x: math::max(0, start.x);
+		for(int y = y1; y <= y2; ++y) {
+			SetVector &row = grid[wrap(y, grid.size())];
+			const int x2 = _wrap? end.x: math::min((int)grid[y].size() - 1, end.x);
+			for(int x = x1; x <= x2; ++x) {
+				row[wrap(x, row.size())].insert(id);
+			}
+		}
+	}
+
+	void resize(GridMatrix &grid, const v2<int> &grid_size, const v2<int> &map_size) {
+		v2<int> dim = (map_size - 1) / grid_size + 1;
+		grid.resize(dim.y);
+		for(int y = 0; y < dim.y; ++y) 
+			grid[y].resize(dim.x);
+	}
+
+	v2<int> _grid_size; 
+	v2<int> _grid4_size;
+	
+	v2<int> _map_size;
+	
+	GridMatrix _grid, _grid4;
+	
 	struct object_hash {
 		inline size_t operator()(const ::Object *o) const { 
 			size_t x = (size_t)o;
@@ -130,10 +218,6 @@ private:
 	};
 	
 	typedef MRT_HASH_MAP <T, Object, object_hash > Index;
-	
-	typedef quad_tree<int, T, C> tree_type; 
-	typedef typename tree_type::point_type value_type;
-	tree_type tree;
 	
 	Index _index;
 	bool _wrap;
