6678a8447ec8b7cc637b7785153002e89fe71872
Vladimir Menshakov <whoozle@yandex.ru>
2008-12-07 02:03:07 +0000

    implemented quad tree for rectangles
    git-svn-id: https://btanks.svn.sourceforge.net/svnroot/btanks/trunk@8001 5c0bf991-d514-0410-a65f-ce4832e571f3

--- math/quad_tree.h
+++ math/quad_tree.h
@@ -30,222 +30,181 @@
 
 #include <set>
 #include <assert.h>
+#include <stdio.h>
 
 template<typename T, typename V>
-struct quad_point {
-	T x, y;
+struct quad_rect {
+	T x0, y0, x1, y1;
 	V value;
 	
-	quad_point() : x(0), y(0), value(0) {}
-	quad_point(const V& v) : x(0), y(0), value(v) {}
-	quad_point(const T& x, const T& y, const V& v) : x(x), y(y), value(v) {}
+	quad_rect() : x0(0), y0(0), x1(0), y1(0), value(0) {}
+	quad_rect(const V& v) : x0(0), y0(0), x1(0), y1(0), value(v) {}
+	quad_rect(const T& x0, const T& y0, const T& x1, const T& y1, const V& v) : x0(x0), y0(y0), x1(x1), y1(y1), value(v) {}
 	
-	inline bool operator<(const quad_point &other) const {
-		if (x != other.x)
-			return x < other.x;
-		return y < other.y;
+	inline bool operator<(const quad_rect &other) const {
+		if (x0 != other.x0)
+			return x0 < other.x0;
+		if (y0 != other.y0)
+			return x0 < other.x0;
+		if (x1 != other.x1)
+			return x1 < other.x1;
+		return y1 < other.y1;
+	}
+
+	inline bool operator==(const quad_rect &other) const {
+		return y0 == other.y0 && y1 == other.y1 && x0 == other.x0 && x1 == other.x1;
+	}
+
+	inline bool valid() const {
+		return x0 <= x1 && y0 <= y1;
+	}
+	
+	bool contains(const quad_rect & other) const {
+		return x0 <= other.x0 && x1 >= other.x1 && y0 <= other.y0 && y1 >= other.y1;
+	}
+
+	inline bool intersects(const quad_rect & other) const {
+		return !( x0 >= other.x1 || x1 <= other.x0 ||
+				y0 >= other.y1 || y1 <= other.y0 );
+	}
+	
+	inline void clear() {
+		x0 = 0;
+		x1 = 0;
+		y0 = 0;
+		y1 = 0;
 	}
 };
 
 template<typename T, typename V, int capacity> 
 struct quad_node {
 	enum { unsplit_capacity = capacity * 2 / 3 };
-	typedef quad_point<T, V> point_type;
+	typedef quad_rect<T, V> rect_type;
 	typedef quad_node<T, V, capacity> node_type;
-	typedef std::multiset<point_type> points_type;
+	typedef std::list<rect_type> rects_type;
 	
-	int start_x, start_y, end_x, end_y;
-	node_type * child[4];
-	points_type points;
 	
+	rect_type area;
+	rects_type rects;
+	quad_node * child[4];
 	size_t children_count;
-
-	inline quad_node(int start_x, int start_y, int end_x, int end_y): 
-		start_x(start_x), start_y(start_y), end_x(end_x), end_y(end_y), points(), children_count(0) {
-			for(int i = 0; i < 4; ++i)
-				child[i] = NULL;
+	
+	quad_node(const rect_type &area) : area(area), rects(), children_count(0) {
+		child[0] = NULL;
+		child[1] = NULL;
+		child[2] = NULL;
+		child[3] = NULL;
+	}
+	~quad_node() {
+		for(int i = 0; i < 4; ++i) {
+			delete child[i];
+			child[i] = NULL;
 		}
-		
-	inline void set_size(const T&w, const T&h) {
+	}
+	
+	void set_size(const T & w, const T & h) {
 		clear();
-		//printf("set_size(%d, %d)\n", w, h);
-		end_x = start_x + w; end_y = start_y + h;
+		area.x1 = w;
+		area.y1 = h;
 	}
-		
-	inline int index(const point_type & point) const {
-		return index(point.x, point.y);
+	
+	void clear() {
+		area.clear();
+		rects.clear();
+		for(int i = 0; i < 4; ++i) {
+			delete child[i];
+			child[i] = NULL;
+		}
+		children_count = NULL;
 	}
+	
+	void search(std::set<V> & result, const rect_type &s_area) const {
+		if (!area.intersects(s_area))
+			return;
 
-	inline int index(int x, int y) const {
-		int w = (end_x - start_x - 1) / 2 + 1;
-		int h = (end_y - start_y - 1) / 2 + 1;
-		y -= start_y;
-		x -= start_x;
-		return (y < h)? 
-			((x < w)? 0: 1): 
-			((x < w)? 2: 3);
-	}
-	
-	inline bool empty() const {
-		return points.empty() && child[0] == NULL;
-	}
-	
-	void unsplit(points_type &parent) {
-		if (child[0] != NULL) {
-			assert(points.empty());
-			for(int i = 0; i < 4; ++i) {
-				child[i]->unsplit(parent);
-				delete child[i];
-				child[i] = NULL;
-			}
-		} else {
-			parent.insert(points.begin(), points.end());
+		if (child[0] != NULL)
+		for(int j = 0; j < 4; ++j) {
+			//if (area.contains(child[j]->area))
+				//move all area to result :)
+			child[j]->search(result, s_area);
+		}
+
+		for(typename rects_type::const_iterator i = rects.begin(); i != rects.end(); ++i) {
+			const rect_type & rect = *i;
+			if (rect.intersects(s_area))
+				result.insert(rect.value);
 		}
 	}
 	
 	void split() {
 		assert(child[0] == NULL);
-		int hx = (end_x - start_x - 1) / 2 + 1;
-		int hy = (end_y - start_y - 1) / 2 + 1;
-		//printf("splitting node position %d,%d,%d,%d\n", start_x, start_y, end_x, end_y);
-		child[0] = new node_type(start_x, start_y, start_x + hx, start_y + hy);
-		child[1] = new node_type(start_x + hx, start_y, end_x, start_y + hy);
-		child[2] = new node_type(start_x, start_y + hy, start_x + hx, end_y);
-		child[3] = new node_type(start_x + hx, start_y + hy, end_x, end_y);
-		for(typename points_type::const_iterator i = points.begin(); i != points.end(); ++i) {
-			const point_type &point = *i;
-			int idx = index(point);
-			node_type * dst_node = child[idx];
-			//printf("putting point into quad %d\n", idx);
-			dst_node->insert(point);
-		}
-		points.clear();
-	}
+		
+		int w = area.x1 - area.x0, h = area.y1 - area.y0;
+		if (w < 2 || h < 2)
+			return;
 
-	size_t insert(const point_type & point) {
-		if (point.x < start_x || point.x >= end_x || point.y < start_y || point.y >= end_y)
+		//printf("splitting node %d,%d,%d,%d (%u)\n", area.x0, area.y0, area.x1, area.y1, (unsigned)children_count);
+		
+		int hx = (w - 1) / 2 + 1;
+		int hy = (h - 1) / 2 + 1;
+		child[0] = new quad_node(rect_type(area.x0, 		area.y0, 		area.x0 + hx, 	area.y0 + hy, NULL));
+		child[1] = new quad_node(rect_type(area.x0 + hx, 	area.y0, 		area.x1, 		area.y0 + hy, NULL));
+		child[2] = new quad_node(rect_type(area.x0, 		area.y0 + hy, 	area.x0 + hx, 	area.y1, NULL));
+		child[3] = new quad_node(rect_type(area.x0 + hx, 	area.y0 + hy, 	area.x1, 		area.y1, NULL));
+	}
+	
+	bool insert(const rect_type & object) {
+		if (!area.contains(object))
 			return false;
 		
-		size_t inc;
-		if (child[0] != NULL) {
-			assert(points.empty());
-			node_type * dst_node = child[index(point)];
-			inc = dst_node->insert(point);
-		} else {
-			typename points_type::iterator i = points.find(point);
-			inc = (i != points.end())?0: 1;
-			points.insert(point);
-		} 
-		children_count += inc;
-
-		if (child[0] == NULL && children_count > capacity) 
+		if (child[0] == NULL) 
 			split();
 
-		return inc;
-	}
-
-	void search(std::set<V> & result, int x0, int y0, int x1, int y1) const {
-		if (child[0] != NULL) {
-			assert(points.empty());
-			int i0 = index(x0, y0);
-			int i1 = index(x1, y1);
-			int x, y, dx, dy;
-			switch(i1 - i0) {
-			case 3: 
-				x = y = 0; dx = dy = 1;
-				break;
-
-			case 2: 
-				x = dx = i0;
-				y = 0;
-				dy = 1;
-				break;
-
-			case 1: 
-				y = dy = i1 / 2;
-				x = 0;
-				dx = 1;
-				break;
-			
-			case 0: 
-				y = dy = i0 / 2;
-				x = dx = i0 % 2;
-				break;
-
-			default: 
-				assert(0);
-				x = y = 0; dx = dy = -1;
-			}
-			
-			//printf("search: quadrants %d-%d\n", i0, i1);
-			for(; y <= dy; ++y) 
-				for(; x <= dx; ++x) {
-					node_type * dst_node = child[y * 2 + x];
-					//printf("search: node %d,%d %d,%d\n", dst_node->pos_x, dst_node->pos_y, dst_node->size_x, dst_node->size_y);
-					dst_node->search(result, x0, y0, x1, y1);
-				}
-		} else {
-			//printf("%d,%d: searching in %u points(%d,%d %d,%d)\n", pos_x, pos_y, (unsigned)points.size(), x0, y0, x1, y1);
-			for(typename points_type::const_iterator i = points.begin(); i != points.end(); ++i) {
-				const point_type &point = *i;
-				if (point.x >= x0 && point.x < x1 && point.y >= y0 && point.y < y1)	{
-					result.insert(point.value);
-				}
-			}
-		}
-	}
-	
-	size_t erase(const point_type & point) {
-		if (point.x < start_x || point.x >= end_x || point.y < start_y || point.y >= end_y)
-			return false;
-		
-		size_t dec;
-		if (child[0] != NULL) {
-			node_type * dst_node = child[index(point)];
-			dec = dst_node->erase(point);
-		} else {
-			typename points_type::iterator i = points.lower_bound(point);
-			typename points_type::iterator e = points.upper_bound(point);
-			dec = 0;
-			while(i != e) {
-				if (i->value == point.value) {
-					dec = 1;
-					points.erase(i++);
-				} else ++i;
-			}
-		}
-		children_count -= dec;
-			
-		if (child[0] != NULL && children_count < unsplit_capacity) 
-			unsplit(points);
-		return dec;
-	}
-	
-	void clear() {
-		start_x = 0;
-		start_y = 0;
-		end_x = 0;
-		end_y = 0;
-		for(int i = 0; i < 4; ++i) {
-			delete child[i];
-			child[i] = NULL;
-		}
-		children_count = 0;
-	}
-	
-	~quad_node() {
+		if (child[0] != NULL)
 		for(int i = 0; i < 4; ++i) 
-			delete child[i];
+			if (child[i]->insert(object)) {
+				++children_count;
+				return true;
+			}
+	
+		//printf("insert %d,%d,%d,%d into %d,%d,%d,%d [%d]\n", object.x0, object.y0, object.x1, object.y1, area.x0, area.y0, area.x1, area.y1, children_count);
+		rects.push_back(object);
+		++children_count;
+		return true;
 	}
+
+	bool erase(const rect_type & object) {
+		if (!area.contains(object))
+			return false;
+
+		if (child[0] != NULL)
+		for(int i = 0; i < 4; ++i) 
+			if (child[i]->erase(object)) {
+				--children_count;
+				return true;
+			}
+		
+		//printf("erase %d,%d,%d,%d into %d,%d,%d,%d [%d]\n", object.x0, object.y0, object.x1, object.y1, area.x0, area.y0, area.x1, area.y1, children_count);
+		for(typename rects_type::iterator i = rects.begin(); i != rects.end(); ++i) {
+			const rect_type &r = *i;
+			if (r == object && r.value == object.value) {
+				i = rects.erase(i);
+				--children_count;
+				return true;
+			}
+		}
+		return false;
+	}
+	
 };
 
 template<typename T, typename V, int capacity>
 class quad_tree {
 public: 
-	typedef quad_point<T, V> point_type;
+	typedef quad_rect<T, V> rect_type;
 	typedef quad_node<T, V, capacity> node_type;
 	
-	inline quad_tree() : root(0, 0, 0, 0) {}
+	inline quad_tree() : root(rect_type()) {}
 	inline void set_size(const T &w, const T &h) {
 		clear();
 		root.set_size(w, h);
@@ -255,18 +214,22 @@ public:
 		root.clear();
 	}
 	
-	inline void search(std::set<V> & result, int x0, int y0, int x1, int y1) const {
-		if (x0 >= x1 || y0 >= y1)
+	inline void search(std::set<V> & result, const rect_type &area) const {
+		if (!area.valid())
 			return;
-		root.search(result, x0, y0, x1, y1);
+		root.search(result, area);
 	}
 	
-	inline size_t insert(const point_type & point) {
-		return root.insert(point);
+	inline void insert(const rect_type & object) {
+		if (!object.valid())
+			return;
+		root.insert(object);
 	}
 
-	size_t erase(const point_type & point) {
-		return root.erase(point);
+	inline void erase(const rect_type & object) {
+		if (!object.valid())
+			return;
+		root.erase(object);
 	}
 	
 protected: 
