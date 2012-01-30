6a3acf4884fc5288f11f69a2a2934da0b9858baa
Vladimir Menshakov <whoozle@yandex.ru>
2012-01-30 23:07:59 +0400

    removed c++ style macro

--- mrt/export_base.h
+++ mrt/export_base.h
@@ -1,7 +1,6 @@
 #ifndef MRT_EXPORT_MACRO_H__
 #define MRT_EXPORT_MACRO_H__
 
-// Shared library support
 #ifdef _WINDOWS
 
 #	pragma warning(disable:4251)  /* needs to have dll-interface used by client */
