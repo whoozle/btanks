fa17e44db366d31c147ad12a13ac6f94d6bfdd39
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-04-07 13:38:18 +0100

    removal of all obsoleted acceleration stuff, ported cmake files to sdl2/sdl2_image

--- sdlx/sdlx.h
+++ sdlx/sdlx.h
@@ -1,15 +1,7 @@
 #ifndef __SDLX__COMMON_H__
 #define __SDLX__COMMON_H__
 
-#ifdef USE_GLSDL
-#	ifdef _WINDOWS
-#		include "wrappers/d3dsdl.h"
-#	else
-#		include "wrappers/glSDL.h"
-#	endif
-#else
-#	include <SDL.h>
-#endif
+#include <SDL.h>
 
 #include "sdlx/export_sdlx.h"
 
